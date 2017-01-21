f487e293543885c0e876966782e42b664fc331b6
stefanks <solntsev@gmail.com>
2017-01-21 12:24:24 -0600

    Update TestChromatogram.cs

--- Test/TestChromatogram.cs
+++ Test/TestChromatogram.cs
@@ -93,6 +93,8 @@ namespace Test
             Assert.AreEqual(6, a.CreateSmoothChromatogram(SmoothingType.None, -10).GetApex(1.5, 2.5).Intensity);
 
             Assert.AreEqual(8, a.FindNearestApex(10).Y);
+            
+            Assert.AreEqual(4, a.GetApex(5, 6).X);
         }
     }
 }
