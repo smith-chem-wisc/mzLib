c36cfe152a016da3e4eb6325c138646a89e5b474
stepa <stepa@ARES>
2017-03-15 14:45:14 -0500

    content

--- TestThermo/TestThermo.cs
+++ TestThermo/TestThermo.cs
@@ -18,44 +18,6 @@ namespace TestThermo
             Environment.CurrentDirectory = TestContext.CurrentContext.TestDirectory;
         }
 
-        //[Test]
-        //public void LoadThermoTest()
-        //{
-        //    ThermoDynamicData a = ThermoDynamicData.InitiateDynamicConnection(@"Shew_246a_LCQa_15Oct04_Andro_0904-2_4-20.RAW");
-        //    Assert.AreEqual(3316, a.NumSpectra);
-        //    Assert.AreEqual(3316, a.NumSpectra);
-
-        //    var scan = a.GetOneBasedScan(53);
-        //    Assert.AreEqual(1.2623333333333333, scan.RetentionTime);
-        //    Assert.AreEqual(1, scan.MsnOrder);
-        //    Assert.AreEqual("+ c ESI Full ms [400.00-2000.00]", scan.ScanFilter);
-
-        //    var spectrum = a.GetOneBasedScan(53).MassSpectrum;
-
-        //    var peak = spectrum.PeakWithHighestY;
-        //    Assert.AreEqual(75501, peak.Intensity);
-
-        //    Assert.AreEqual(1, spectrum.FilterByY(7.5e4, double.MaxValue).Count());
-        //    Assert.AreEqual(2, spectrum.Extract(new DoubleRange(923, 928)).Count());
-
-        //    Assert.AreEqual(double.NaN, spectrum.GetSignalToNoise(1));
-
-        //    Assert.AreEqual("1.3", a.ThermoGlobalParams.pbstrInstSoftwareVersion);
-        //    var ms2scan = a.GetOneBasedScan(948) as IMsDataScanWithPrecursor<ThermoSpectrum>;
-        //    Assert.IsNull(ms2scan.SelectedIonGuessChargeStateGuess);
-        //    var precursorScan = a.GetOneBasedScan(ms2scan.OneBasedPrecursorScanNumber);
-        //    ms2scan.RecomputeChargeState(precursorScan.MassSpectrum, 0.1, 4);
-        //    Assert.AreEqual(1, ms2scan.SelectedIonGuessChargeStateGuess);
-        //    Assert.IsNull(ms2scan.SelectedIonGuessIntensity);
-        //    ms2scan.RecomputeSelectedPeak(precursorScan.MassSpectrum);
-        //    Assert.AreEqual(4125760, ms2scan.SelectedIonGuessIntensity);
-
-        //    Assert.AreEqual("LCQ", a.ThermoGlobalParams.pbstrInstName);
-        //    Assert.AreEqual("LCQ", a.ThermoGlobalParams.pbstrInstModel);
-
-        //    Assert.AreEqual(false, a.ThermoGlobalParams.MonoisotopicselectionEnabled);
-        //}
-
         [Test]
         public void ThermoLoadError()
         {
