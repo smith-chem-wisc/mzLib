b74f795dcc3c2fb026e9f978a39823e809cd5178
Anthony <anthony.cesnik@gmail.com>
2017-08-09 15:14:28 -0500

    compare ids to determine distinct modifications

--- Proteomics/Modification.cs
+++ Proteomics/Modification.cs
@@ -36,12 +36,14 @@ namespace Proteomics
         public override bool Equals(object o)
         {
             Modification m = o as Modification;
-            return o != null;
+            return o != null 
+                && m.id == this.id 
+                && m.modificationType == this.modificationType;
         }
 
         public override int GetHashCode()
         {
-            return 0;
+            return id.GetHashCode();
         }
 
         #endregion Public Methods
