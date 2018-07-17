c21705ed24bcb1569158b8d79c442918e73f0416
trishorts <mshort@chem.wisc.edu>
2018-07-17 10:37:11 -0500

    new dissassociation type class and collection

--- MassSpectrometry/ProductType.cs
+++ MassSpectrometry/ProductType.cs
@@ -2,12 +2,36 @@ namespace MassSpectrometry
 {
     public enum ProductType
     {
+        //Ion Type      Neutral Mr
+        //a             [N]+[M]-CHO
+        //a*	        a-NH3
+        //a°	        a-H2O
+        //b             [N]+[M]-H
+        //b*	        b-NH3
+        //b°	        b-H2O
+        //c             [N]+[M]+NH2
+        //d             a – partial side chain
+        //v             y – complete side chain
+        //w             z – partial side chain
+        //x             [C]+[M]+CO-H
+        //y             [C]+[M]+H
+        //y*	        y-NH3
+        //y°	        y-H2O
+        //z             [C]+[M]-NH2
+
+        A,
+        Astar,
         Adot,
         B,
+        Bstar,
+        Bdot,
         BnoB1ions,
         C,
         X,
         Y,
+        Ystar,
+        Ydot,
+        Z,
         Zdot,
         None
     }
