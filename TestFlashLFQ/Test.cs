073c55a7ad1fd78bdb74233ae1265938c319d065
rob <rmillikin@wisc.edu>
2018-08-27 16:56:50 -0500

    MBR protein quant refining

--- TestFlashLFQ/Test.cs
+++ TestFlashLFQ/Test.cs
@@ -14,7 +14,7 @@ namespace Test
     internal class Test
     {
         [Test]
-        public static void TestFlashLFQ()
+        public static void TestFlashLfq()
         {
             // get the raw file paths
             SpectraFileInfo raw = new SpectraFileInfo(Path.Combine(TestContext.CurrentContext.TestDirectory, @"sliced-raw.raw"), "a", 0, 0, 0);
@@ -332,6 +332,98 @@ namespace Test
 
             Assert.That(results.peaks[file1].Count == 5);
             Assert.That(results.peaks[file1].Where(p => p.IsMbrFeature == true).Count() == 0);
+
+            Assert.That(results.proteinGroups["MyProtein"].GetIntensity(file1) > 0);
+            Assert.That(results.proteinGroups["MyProtein"].GetIntensity(file2) > 0);
+        }
+
+        [Test]
+        public static void TestFlashLfqMatchBetweenRunsProteinQuant()
+        {
+            List<string> filesToWrite = new List<string> { "mzml_1", "mzml_2" };
+            List<string> pepSequences = new List<string> { "PEPTIDE", "PEPTIDEV", "PEPTIDEVV", "PEPTIDEVVV", "PEPTIDEVVVV" };
+            double intensity = 1e6;
+
+            double[] file1Rt = new double[] { 1.01, 1.02, 1.03, 1.04, 1.05 };
+            double[] file2Rt = new double[] { 1.015, 1.030, 1.036, 1.050, 1.065 };
+
+            Loaders.LoadElements(Path.Combine(TestContext.CurrentContext.TestDirectory, @"elements.dat"));
+
+            // generate mzml files (5 peptides each)
+            for (int f = 0; f < filesToWrite.Count; f++)
+            {
+                // 1 MS1 scan per peptide
+                MsDataScan[] scans = new MsDataScan[5];
+
+                for (int p = 0; p < pepSequences.Count; p++)
+                {
+                    ChemicalFormula cf = new Proteomics.AminoAcidPolymer.Peptide(pepSequences[p]).GetChemicalFormula();
+                    IsotopicDistribution dist = IsotopicDistribution.GetDistribution(cf, 0.125, 1e-8);
+                    double[] mz = dist.Masses.Select(v => v.ToMz(1)).ToArray();
+                    double[] intensities = dist.Intensities.Select(v => v * intensity).ToArray();
+                    double rt;
+                    if (f == 0)
+                    {
+                        rt = file1Rt[p];
+                    }
+                    else
+                    {
+                        rt = file2Rt[p];
+                    }
+
+                    // add the scan
+                    scans[p] = new MsDataScan(massSpectrum: new MzSpectrum(mz, intensities, false), oneBasedScanNumber: p + 1, msnOrder: 1, isCentroid: true,
+                        polarity: Polarity.Positive, retentionTime: rt, scanWindowRange: new MzRange(400, 1600), scanFilter: "f",
+                        mzAnalyzer: MZAnalyzerType.Orbitrap, totalIonCurrent: intensities.Sum(), injectionTime: 1.0, noiseData: null, nativeId: "scan=" + (p + 1));
+                }
+
+                // write the .mzML
+                IO.MzML.MzmlMethods.CreateAndWriteMyMzmlWithCalibratedSpectra(new FakeMsDataFile(scans),
+                    Path.Combine(TestContext.CurrentContext.TestDirectory, filesToWrite[f] + ".mzML"), false);
+            }
+
+            // set up spectra file info
+            SpectraFileInfo file1 = new SpectraFileInfo(Path.Combine(TestContext.CurrentContext.TestDirectory, filesToWrite[0] + ".mzML"), "a", 0, 0, 0);
+            SpectraFileInfo file2 = new SpectraFileInfo(Path.Combine(TestContext.CurrentContext.TestDirectory, filesToWrite[1] + ".mzML"), "a", 1, 0, 0);
+
+            // create some PSMs
+            var pg = new ProteinGroup("MyProtein", "gene", "org");
+            var myMbrProteinGroup = new ProteinGroup("MyMbrProtein", "MbrGene", "org");
+
+            Identification id1 = new Identification(file1, "PEPTIDE", "PEPTIDE",
+                new Proteomics.AminoAcidPolymer.Peptide("PEPTIDE").MonoisotopicMass, file1Rt[0] + 0.001, 1, new List<ProteinGroup> { pg });
+            Identification id2 = new Identification(file1, "PEPTIDEV", "PEPTIDEV",
+                new Proteomics.AminoAcidPolymer.Peptide("PEPTIDEV").MonoisotopicMass, file1Rt[1] + 0.001, 1, new List<ProteinGroup> { pg });
+            Identification id3 = new Identification(file1, "PEPTIDEVV", "PEPTIDEVV",
+                new Proteomics.AminoAcidPolymer.Peptide("PEPTIDEVV").MonoisotopicMass, file1Rt[2] + 0.001, 1, new List<ProteinGroup> { myMbrProteinGroup });
+            Identification id4 = new Identification(file1, "PEPTIDEVVV", "PEPTIDEVVV",
+                new Proteomics.AminoAcidPolymer.Peptide("PEPTIDEVVV").MonoisotopicMass, file1Rt[3] + 0.001, 1, new List<ProteinGroup> { pg });
+            Identification id5 = new Identification(file1, "PEPTIDEVVVV", "PEPTIDEVVVV",
+                new Proteomics.AminoAcidPolymer.Peptide("PEPTIDEVVVV").MonoisotopicMass, file1Rt[4] + 0.001, 1, new List<ProteinGroup> { pg });
+
+            Identification id6 = new Identification(file2, "PEPTIDE", "PEPTIDE",
+                new Proteomics.AminoAcidPolymer.Peptide("PEPTIDE").MonoisotopicMass, file2Rt[0] + 0.001, 1, new List<ProteinGroup> { pg });
+            Identification id7 = new Identification(file2, "PEPTIDEV", "PEPTIDEV",
+                new Proteomics.AminoAcidPolymer.Peptide("PEPTIDEV").MonoisotopicMass, file2Rt[1] + 0.001, 1, new List<ProteinGroup> { pg });
+            // missing ID 8 - MBR feature
+            Identification id9 = new Identification(file2, "PEPTIDEVVV", "PEPTIDEVVV",
+                new Proteomics.AminoAcidPolymer.Peptide("PEPTIDEVVV").MonoisotopicMass, file2Rt[3] + 0.001, 1, new List<ProteinGroup> { pg });
+            Identification id10 = new Identification(file2, "PEPTIDEVVVV", "PEPTIDEVVVV",
+                new Proteomics.AminoAcidPolymer.Peptide("PEPTIDEVVVV").MonoisotopicMass, file2Rt[4] + 0.001, 1, new List<ProteinGroup> { pg });
+
+            // test with top3 protein quant engine
+            FlashLFQEngine engine = new FlashLFQEngine(new List<Identification> { id1, id2, id3, id4, id5, id6, id7, id9, id10 }, matchBetweenRuns: true);
+            var results = engine.Run();
+            
+            Assert.That(results.proteinGroups["MyMbrProtein"].GetIntensity(file1) > 0);
+            Assert.That(results.proteinGroups["MyMbrProtein"].GetIntensity(file2) == 0);
+            
+            // test with advanced protein quant engine
+            engine = new FlashLFQEngine(new List<Identification> { id1, id2, id3, id4, id5, id6, id7, id9, id10 }, matchBetweenRuns: true, advancedProteinQuant: true);
+            results = engine.Run();
+
+            Assert.That(results.proteinGroups["MyMbrProtein"].GetIntensity(file1) > 0);
+            Assert.That(results.proteinGroups["MyMbrProtein"].GetIntensity(file2) == 0);
         }
     }
 }
