f28868b5f05c1304ddf77133b9d2660cc627fad8
Leah Schaffer <lschaffer2@wisc.edu>
2018-09-06 20:09:01 -0500

    original ID mod bug fixes (#395)

--- UsefulProteomicsDatabases/PtmListLoaderGeneral.cs
+++ UsefulProteomicsDatabases/PtmListLoaderGeneral.cs
@@ -291,13 +291,7 @@ namespace UsefulProteomicsDatabases
                                             _monoisotopicMass = AdjustMonoIsotopicMassForFormalCharge(_monoisotopicMass, _chemicalFormula, _databaseReference, formalChargesDictionary);
                                         }
 
-                                        string idWithMotifAppended = _id;
-                                        if (_id != null && motif != null && !_id.Contains(" on ") && !_id.Contains(" of "))
-                                        {
-                                            idWithMotifAppended = _id + " on " + motif.ToString();
-                                        }
-
-                                        yield return new Modification(idWithMotifAppended, _accession, _modificationType, _featureType, motif, _locationRestriction, _chemicalFormula, _monoisotopicMass, _databaseReference, _taxonomicRange, _keywords, _neutralLosses, _diagnosticIons, _fileOrigin);
+                                        yield return new Modification(_id, _accession, _modificationType, _featureType, motif, _locationRestriction, _chemicalFormula, _monoisotopicMass, _databaseReference, _taxonomicRange, _keywords, _neutralLosses, _diagnosticIons, _fileOrigin);
                                     }
                                 }
                                 else
