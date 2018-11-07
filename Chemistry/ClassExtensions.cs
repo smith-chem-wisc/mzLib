4533ab0732b05fa83a6f8f376895050b5d43a973
trishorts <mshort@chem.wisc.edu>
2018-11-07 10:11:12 -0600

    removed handy but unused extension method

--- Chemistry/ClassExtensions.cs
+++ Chemistry/ClassExtensions.cs
@@ -57,12 +57,6 @@ namespace Chemistry
             return myNumber;
         }
 
-        public static IEnumerable<List<T>> Partition<T>(this IList<T> source, Int32 size)
-        {
-            for (int i = 0; i < Math.Ceiling(source.Count / (Double)size); i++)
-                yield return new List<T>(source.Skip(size * i).Take(size));
-        }
-
         public class TupleList<T1, T2> : List<Tuple<T1, T2>>
         {
             public void Add(T1 item, T2 item2)
