3b454f02f2901eb82360ac157a04e9aad9f7cf54
trishorts <mshort@chem.wisc.edu>
2019-03-12 07:55:12 -0500

    small unit tests

---
+++ Test/TestMoreProtease.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NUnit.Framework;
+using Proteomics.ProteolyticDigestion;
+
+namespace Test
+{
+    [TestFixture]
+    public static class TestMoreProtease
+    {
+        [Test]
+        public static void SomeAdditionalProteaseTests()
+        {
+            Protease myProtease = new Protease("name", CleavageSpecificity.None, "psiAccessionNumber", "psiName", new List<DigestionMotif>());
+
+            Assert.AreEqual(Proteomics.ProteolyticDigestion.CleavageSpecificity.None, myProtease.GetCleavageSpecificity("SEQUENCE",0,7));
+
+
+        }
+    }
+}
