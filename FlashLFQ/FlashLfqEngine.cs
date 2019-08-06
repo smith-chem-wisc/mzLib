f8d2a765230617f44bee2d034b101d695793cce1
rob <rmillikin@wisc.edu>
2019-08-06 16:10:23 -0500

    fix parameter

--- FlashLFQ/FlashLfqEngine.cs
+++ FlashLFQ/FlashLfqEngine.cs
@@ -204,7 +204,7 @@ namespace FlashLFQ
             _results.CalculatePeptideResults();
 
             // do top3 protein quantification
-            _results.CalculateProteinResultsTop3();
+            _results.CalculateProteinResultsTop3(UseSharedPeptidesForProteinQuant);
 
             // do Bayesian protein fold-change analysis
             if (BayesianProteinQuant)
