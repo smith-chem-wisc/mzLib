67d8f46cb50aa2361d310537d0e50cabac8249dd
trishorts <mshort@chem.wisc.edu>
2020-02-11 14:00:09 -0600

    update pep xml to version 122

---
+++ Test/TestDeconvolution.cs
@@ -0,0 +1,58 @@
+﻿using Chemistry;
+using MassSpectrometry;
+using MzLibUtil;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Test
+{
+    [TestFixture]
+    public sealed class TestDeconvolution
+    {
+        [Test]
+        [TestCase(586.2143122,24, 41983672, 586.2)]//This is a lesser abundant charge state envelope at the low mz end
+        [TestCase(740.372202090153, 19, 108419280, 740.37)]//This is the most abundant charge state envelope
+        [TestCase(1081.385183, 13, 35454636, 1081.385)]//This is a lesser abundant charge state envelope at the high mz end
+        public void TestDeconvolutionProteoformMultiChargeState(double selectedIonMz, int selectedIonChargeStateGuess, double selectedIonIntensity, double isolationMz)
+        {
+            MsDataScan[] Scans = new MsDataScan[1];
+
+            string Ms1SpectrumPath = Path.Combine(TestContext.CurrentContext.TestDirectory, @"DataFiles\14kDaProteoformMzIntensityMs1.txt");
+
+            string[] spectrumLines = File.ReadAllLines(Ms1SpectrumPath);
+
+            int mzIntensityPairsCount = spectrumLines.Length;
+            double[] ms1mzs = new double[mzIntensityPairsCount];
+            double[] ms1intensities = new double[mzIntensityPairsCount];
+
+            for (int i = 0; i < mzIntensityPairsCount; i++)
+            {
+                string[] pair = spectrumLines[i].Split('\t');
+                ms1mzs[i] = Convert.ToDouble(pair[0]);
+                ms1intensities[i] = Convert.ToDouble(pair[1]);
+            }
+
+            MzSpectrum MS1 = new MzSpectrum(ms1mzs, ms1intensities, false);
+
+            Scans[0] = new MsDataScan(MS1, 1, 1, false, Polarity.Positive, 1.0, new MzRange(495, 1617), "first spectrum", MZAnalyzerType.Unknown, MS1.SumOfAllY, null, null, null, selectedIonMz, selectedIonChargeStateGuess, selectedIonIntensity, isolationMz, 4);
+
+            var myMsDataFile = new FakeMsDataFile(Scans);
+
+            var cool = myMsDataFile.GetAllScansList()[0];
+
+            int maxAssumedChargeState = 40;
+            Tolerance massTolerance = Tolerance.ParseToleranceString("10 PPM");
+
+            List<IsotopicEnvelope> isolatedMasses = cool.GetIsolatedMassesAndCharges(cool.MassSpectrum, 1, maxAssumedChargeState, 10, 5).ToList();
+
+            List<double> monoIsotopicMasses = isolatedMasses.Select(m => m.monoisotopicMass).ToList();
+
+            //The primary monoisotopic mass should be the same regardless of which peak in which charge state was selected for isolation.
+            //this case is interesting because other monoisotopic mass may have a sodium adduct. The unit test could be expanded to consider this.
+            Assert.That(monoIsotopicMasses[0], Is.EqualTo(14037.94).Within(.05));
+        }
+    }
+}
