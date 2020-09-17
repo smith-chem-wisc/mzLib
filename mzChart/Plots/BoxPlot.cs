9cca3e4612898465a0636eb7b99840e05394a611
rob <rmillikin@wisc.edu>
2020-09-17 14:03:19 -0500

    skeleton for boxplot

---
+++ mzChart/Plots/BoxPlot.cs
@@ -0,0 +1,16 @@
+﻿using MzLibUtil;
+using OxyPlot.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace mzPlot
+{
+    public class BoxPlot : Plot
+    {
+        public BoxPlot(PlotView oxyPlotView, List<Datum> data) : base(oxyPlotView)
+        {
+            AddHistogram(data);
+        }
+    }
+}
