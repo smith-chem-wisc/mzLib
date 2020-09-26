0eb71cff35eb461c7274d46f9fd739727b21427d
Robert Millikin <rmillikin@wisc.edu>
2020-09-26 12:52:25 -0500

    easier charting (mzPlot) (#547)
    * easier charting
    
    * unit tests
    
    * couple more tests
    
    * export to pdf option, XIC stuff, stash some other temp changes
    
    * skeleton for heatmap
    
    * skeleton for boxplot
    
    * add text annotation option
    
    * clean up a bit, comments, png export
    
    * better looking histogram
    
    * done except for unit tests
    
    * unit tests
    
    * fix unit test
    
    * add png and pdf export unit tests
    
    * add some more unit tests, removed some unimplemented plot skeletons for code cov reasons
    
    * Delete WpfApp1.csproj
    
    * Delete MainWindow.xaml.cs
    
    * Delete App.xaml
    
    * Delete MainWindow.xaml
    
    * Delete App.xaml.cs
    
    * Delete AssemblyInfo.cs
    
    * add oxyplot to nuget dependencies, more unit testing
    
    * svg export; auto bind model to plotview
    
    * changes for review comments; moved XIC extraction to MsDataFile
    
    * fix unit test

---
+++ mzPlot/Plots/SpectrumPlot.cs
@@ -0,0 +1,27 @@
+﻿using BayesianEstimation;
+using MassSpectrometry;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using MzLibUtil;
+using System.Linq;
+using OxyPlot;
+using OxyPlot.Axes;
+
+namespace mzPlot
+{
+    public class SpectrumPlot : Plot
+    {
+        /// <summary>
+        /// Adds a spectrum plot. The data X value is the X-coordinate of the spectral line, the data Y value is the height of 
+        /// the spectral line at X.
+        /// </summary>
+        public SpectrumPlot(OxyPlot.Wpf.PlotView oxyPlotView, IEnumerable<Datum> data, OxyColor? lineColor = null, double lineThickness = 0.5,
+            string xAxisLabel = null, string yAxisLabel = null, string chartTitle = null, string chartSubtitle = null,
+            bool addToLegend = true, string seriesTitle = null, bool refreshAfterAddingData = true) : base(oxyPlotView)
+        {
+            AddSpectrumPlot(data, lineColor, lineThickness, xAxisLabel, yAxisLabel, chartTitle, chartSubtitle,
+                addToLegend, seriesTitle, refreshAfterAddingData);
+        }
+    }
+}
