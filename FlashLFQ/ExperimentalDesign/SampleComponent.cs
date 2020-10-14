2c5e408bf4d3f844b422a48650ec31ce3ce5df94
rob <rmillikin@wisc.edu>
2020-10-14 15:51:38 -0500

    just some test stuff

---
+++ FlashLFQ/ExperimentalDesign/SampleComponent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlashLFQ
+{
+    public class SampleComponent
+    {
+        public readonly string SampleGroup;
+        public readonly int Sample;
+        public readonly int Fraction;
+        public readonly int Replicate;
+
+        public readonly SampleType SampleType;
+        public string Label;
+        public double LabelMass;
+
+        public SampleComponent()
+        {
+
+        }
+    }
+}
