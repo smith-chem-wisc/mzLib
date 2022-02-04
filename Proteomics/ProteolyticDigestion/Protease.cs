3140e4d28a9489f8ab5ecac04455aee06ff4e35d
MICHAEL SHORTREED <mrshortreed@wisc.edu>
2022-02-04 13:18:09 -0600

    remove unused test code

--- Proteomics/ProteolyticDigestion/Protease.cs
+++ Proteomics/ProteolyticDigestion/Protease.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 
 namespace Proteomics.ProteolyticDigestion
@@ -146,7 +145,6 @@ namespace Proteomics.ProteolyticDigestion
             else if (CleavageSpecificity == CleavageSpecificity.Semi)
             {
                 peptides.AddRange(SemiProteolyticDigestion(protein, initiatorMethionineBehavior, maximumMissedCleavages, minPeptideLength, maxPeptideLength));
-                File.WriteAllLines(@"C:\Users\mrsho\Documents\junk\biomarkers.txt", peptides.Select(p => p.BaseSequence).ToArray());
             }
             else
             {
