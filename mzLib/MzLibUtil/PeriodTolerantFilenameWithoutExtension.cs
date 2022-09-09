1fd4434509e7b7cd1b361b3f61f0f94d69157d7f
MICHAEL SHORTREED <mrshortreed@wisc.edu>
2022-09-09 12:20:14 -0500

    add period tolerant get filename utility

---
+++ mzLib/MzLibUtil/PeriodTolerantFilenameWithoutExtension.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+
+namespace MzLibUtil
+{
+    public class PeriodTolerantFilenameWithoutExtension
+    {
+        public static string GetPeriodTolerantFilenameWithoutExtension(string filepathAndOrName)
+        {
+            string windowsSplit = filepathAndOrName.Split('\\').Last();
+            string linuxSplit = filepathAndOrName.Split('/').Last();
+
+            if(windowsSplit.Length <= linuxSplit.Length)
+            {
+                int periodBeforeExtension = windowsSplit.LastIndexOf('.');
+                if (periodBeforeExtension > 0)
+                {
+                    return windowsSplit[..periodBeforeExtension];
+                }
+                else
+                {
+                    return windowsSplit;
+                }  
+            }
+            else
+            {
+                int periodBeforeExtension = linuxSplit.LastIndexOf('.');
+                if (periodBeforeExtension > 0)
+                {
+                    return linuxSplit[..periodBeforeExtension];
+                }
+                else
+                {
+                    return linuxSplit;
+                }
+            }
+        }
+    }
+}
