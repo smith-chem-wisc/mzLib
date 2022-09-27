06815395d4b5dbca9de6c35955c222d3a86ac00d
avcarr2 <64652734+avcarr2@users.noreply.github.com>
2022-09-27 17:26:16 -0500

    Updated ThermoRawFileReader

--- mzLib/Readers/IDataReader.cs
+++ mzLib/Readers/IDataReader.cs
@@ -1,51 +1,14 @@
-﻿using Chemistry;
-using MzLibUtil;
+﻿using MzLibUtil;
 using MassSpectrometry; 
 namespace Readers
 {
-    // only loads file.
     public interface IDataReader
     {
-        public string FilePath { get; }
-        public void LoadAllStaticData(FilteringParams filteringParams = null, int maxThreads = 1);
-
-        public SourceFile GetSourceFile(); 
-    }
-
-    public static class MsDataFileHelpers
-    {
-        
-        /// <summary>
-        /// Extracts an ion chromatogram from the spectra file, given a mass, charge, retention time, and mass tolerance.
-        /// </summary>
-        public static ExtractedIonChromatogram ExtractIonChromatogram(this MsDataFile file, double neutralMass, int charge, Tolerance massTolerance,
-            double retentionTimeInMinutes, int msOrder = 1, double retentionTimeWindowWidthInMinutes = 5)
-        {
-            double theorMz = neutralMass.ToMz(charge);
-            double startRt = retentionTimeInMinutes - retentionTimeWindowWidthInMinutes / 2;
-            double endRt = retentionTimeInMinutes + retentionTimeWindowWidthInMinutes / 2;
-            List<Datum> xicData = new List<Datum>();
-
-            IEnumerable<MsDataScan> scansInRtWindow = file.GetMsScansInTimeRange(startRt, endRt);
-
-            foreach (MsDataScan scan in scansInRtWindow.Where(p => p.MsnOrder == msOrder))
-            {
-                int ind = scan.MassSpectrum.GetClosestPeakIndex(theorMz);
-
-                double expMz = scan.MassSpectrum.XArray[ind];
-
-                if (massTolerance.Within(expMz.ToMass(charge), neutralMass))
-                {
-                    xicData.Add(new Datum(scan.RetentionTime, scan.MassSpectrum.YArray[ind]));
-                }
-                else
-                {
-                    xicData.Add(new Datum(scan.RetentionTime, 0));
-                }
-            }
-
-            return new ExtractedIonChromatogram(xicData);
-        }
-
+        MsDataScan[] Scans { get; }
+        SourceFile SourceFile { get; }
+        public List<MsDataScan> LoadAllScansFromFile(string filepath, 
+            IFilteringParams filteringParams, 
+            int maxThreads = -1); 
+        public SourceFile GetSourceFile(string filepath);
     }
 }
