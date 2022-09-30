d2d093de62355d363b9de55156539ee9819c5cda
avcarr2 <64652734+avcarr2@users.noreply.github.com>
2022-09-30 16:40:13 -0500

    * fixing tests and code after overhauling the scan readers

---
+++ mzLib/Readers/GenericMsDataFile.cs
@@ -0,0 +1,49 @@
+﻿using MassSpectrometry;
+
+namespace Readers;
+
+public class GenericMsDataFile : MsDataFile
+{
+    public override void LoadAllStaticData(FilteringParams filteringParams = null, int maxThreads = 1)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override SourceFile GetSourceFile()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override MsDataScan GetOneBasedScanFromDynamicConnection(int oneBasedScanNumber, IFilteringParams filterParams = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void CloseDynamicConnection()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void InitiateDynamicConnection()
+    {
+        throw new NotImplementedException();
+    }
+    public GenericMsDataFile(int numSpectra, SourceFile sourceFile) : base(numSpectra, sourceFile)
+    {
+
+    }
+    public GenericMsDataFile(MsDataScan[] scans, SourceFile sourceFile) : base(scans, sourceFile)
+    {
+
+    }
+
+    public GenericMsDataFile() : base()
+    {
+
+    }
+
+    public GenericMsDataFile(string filePath) : base(filePath)
+    {
+
+    }
+}
---
+++ mzLib/Readers/ReaderCreator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MzLibUtil;
+
+namespace Readers
+{
+    public static class ReaderCreator
+    {
+        public static MsDataFile CreateReader(string filePath)
+        {
+            string fileExtension = Path.GetExtension(filePath).ToLowerInvariant();
+            IReaderFactory factory = null; 
+            switch (fileExtension)
+            {
+                case ".raw":
+                    factory = new ThermoRawReaderFactory(filePath); 
+                    break;
+                case ".d":
+                    throw new NotImplementedException();
+                case ".mzml":
+                    factory = new MzMLReaderFactory(filePath); 
+                    break;
+                case ".mgf":
+                    factory = new MgfReaderFactory(filePath); 
+                    break;
+                default:
+                    throw new MzLibException("File extension not supported."); 
+            }
+            return factory.Reader; 
+        }
+    }
+}
