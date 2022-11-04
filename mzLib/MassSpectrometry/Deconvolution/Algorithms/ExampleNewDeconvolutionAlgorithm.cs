3cb60c1dceea66f6325ca7166e7828d837fdf769
nbollis <nbollis@comcast.net>
2022-11-04 13:31:02 -0500

    Deconvolution Refactoring (#668)
    * Changed x64;AnyCPU to only AnyCPU in all project files
    
    * One more change
    
    * Nuget - 3.
    Me - 0
    
    * Built structure for deconvolution refactoring
    
    * Implemented Exact same test for original deconvolution but for the new structure
    
    * Commented the Code
    
    * Refactored to have unique deconvolution parameters for each algorithm
    
    * Fixed that space

---
+++ mzLib/MassSpectrometry/Deconvolution/Algorithms/ExampleNewDeconvolutionAlgorithm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MzLibUtil;
+
+namespace MassSpectrometry
+{
+    [ExcludeFromCodeCoverage]
+    public class ExampleNewDeconvolutionAlgorithm : DeconvolutionAlgorithm
+    {
+        public ExampleNewDeconvolutionAlgorithm(DeconvolutionParameters deconParameters) : base(deconParameters)
+        {
+
+        }
+
+        public override IEnumerable<IsotopicEnvelope> Deconvolute(MzSpectrum spectrum)
+        {
+            var deconParams = DeconvolutionParameters as ExampleNewDeconvolutionParameters ?? throw new MzLibException("Deconvolution params and algorithm do not match");
+            throw new NotImplementedException();
+        }
+
+    }
+}
