db1c7a849ac418add91e33e7b6a0b19d93b13292
Alex <AlexSolivais@gmail.com>
2022-11-06 15:55:01 -0600

    Added fields to SpectralDeconParams

--- mzLib/MassSpectrometry/Deconvolution/Parameters/SpectralDeconvolutionParameters.cs
+++ mzLib/MassSpectrometry/Deconvolution/Parameters/SpectralDeconvolutionParameters.cs
@@ -3,18 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Proteomics;
+using MassSpectrometry.Proteomics;
+using MassSpectrometry.Proteomics.ProteolyticDigestion;
 
 namespace MassSpectrometry.Deconvolution.Parameters
 {
+
     public class SpectralDeconvolutionParameters : DeconvolutionParameters
     {
-        private List<Protein> Proteins;
-        private bool FindNonDatabasePeaks;
+        public List<Protein> Proteins;
+        public List<Modification> FixedModifications;
+        public List<Modification> VariableModifications;
+        public Protease Protease;
+        private bool FindNonDatabasePeaks; // This should be linked to a method that generates Averagine envelopes
 
-        public SpectralDeconvolutionParameters() : base()
+
+        public SpectralDeconvolutionParameters(int minAssumedChargeState, int maxAssumedChargeState,
+            double deconvolutionTolerancePpm, List<Protein> proteins, List<Modification> fixedModifications,
+            List<Modification> variableModifications, Protease protease, bool findNonDatabasePeaks = false) :
+            base(minAssumedChargeState, maxAssumedChargeState, deconvolutionTolerancePpm)
         {
-
+            Proteins = proteins;
+            FixedModifications = fixedModifications;
+            VariableModifications = variableModifications;
+            Protease = protease;
+            FindNonDatabasePeaks = findNonDatabasePeaks;
         }
     }
 }
