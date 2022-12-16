6b811025b1757a781b6eead6f230949f62ad5427
Alex <AlexSolivais@gmail.com>
2022-12-15 18:10:32 -0600

    Finished the spectrum tree and associated enumerator

--- mzLib/MzLibUtil/TreeDictionary.cs
+++ mzLib/MzLibUtil/TreeDictionary.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -8,49 +9,30 @@ using System.Threading.Tasks;
 
 namespace MzLibUtil
 {
-    // Implemented as a basic 
-    public class TreeDictionary
+    // Implemented as a basic binary search tree, where nodes are ordered according to 
+    // a key double (generally, an m/z value) and a value (generally, the corresponding intensity value)
+    // This affords efficient search, deletion, and insertion ( O(logn) )
+    // Keeping the "key" as a double allows operations such as FindNearest
+    public class SpectrumTree : IEnumerable
     {
-        private Node root;
+        private Node _root;
+        public Node Root => _root;
 
-        internal class Node
+        public SpectrumTree()
         {
-            // Theoretically the key could be any IComparable, but then FindNearest wouldn't be possible
-            private double _key;
-            internal double Key => _key;
-            private Object _value;
-            public Object Value => _value;
-
-            internal Node parent;
-            internal Node leftChild;
-            internal Node rightChild;
-            internal double inOrderSuccesor;
-            internal double inOrderPredecessor;
-
-            public Node(double key, Object value)
-            {
-                _key = key;
-                _value = value;
-            }
-
-            internal void SwapKeyValuePair(double newKey, Object newValue)
-            {
-                _key = newKey;
-                _value = newValue;
-            }
+            _root = null;
         }
 
-        private void Insert(Node newNode)
+        public void Insert(Node newNode)
         {
-            if (root == null)
+            if (Root == null)
             {
-                root = newNode;
-                root.color = Color.Black;
+                _root = newNode;
                 return;
             }
 
             Node y = null;
-            Node x = root;
+            Node x = Root;
             while (x != null)
             {
                 y = x;
@@ -75,11 +57,11 @@ namespace MzLibUtil
             }
         }
 
-        private void Delete(Node node)
+        public void Delete(Node node)
         {
             if (node.parent == null)
             {
-                root = null;
+                _root = null;
                 return;
             }
 
@@ -134,11 +116,14 @@ namespace MzLibUtil
             }
         }
 
-        private Node GetInOrderSuccesor(Node predeccesor)
+        internal static Node GetInOrderSuccesor(Node predeccesor)
         {
             if (predeccesor.rightChild != null)
             {
                 return GetMinNode(predeccesor.rightChild);
+            } else if (predeccesor.parent.rightChild == predeccesor)
+            {
+                return null;
             }
 
             Node parent = predeccesor.parent;
@@ -151,7 +136,7 @@ namespace MzLibUtil
             return parent;
         }
 
-        private Node GetMinNode(Node parent)
+        internal static Node GetMinNode(Node parent)
         {
             Node current = parent;
 
@@ -163,15 +148,98 @@ namespace MzLibUtil
             return current;
         }
 
+        public IEnumerator GetEnumerator()
+        {
+            return new TreeEnumerator(_root);
+        }
+
+        //public List<Node> GetNodeList()
+        //{
+        //    List<Node> allNodes = new();
+        //    Node currentNode = GetMinNode(Root);
+        //    while (currentNode != null)
+        //    {
+        //        allNodes.Add(currentNode);
+        //        currentNode = GetInOrderSuccesor(currentNode);
+        //    }
+        //    return allNodes;
+        //}
+
         public void BuildTreeFromSpectrum(double[] mzArray, double[] intensityArray)
         {
             int middleIndex = mzArray.Length / 2;
-
         }
 
         private void BuildTreeHelper(double[] mzArray, double[] intensityArray, int middleIndex)
         {
 
         }
+    }
+
+    public class Node
+    {
+        // Theoretically the key could be any IComparable, but then FindNearest wouldn't be possible
+        private double _key;
+        internal double Key => _key;
+        private double _value;
+        public double Value => _value;
+
+        internal Node parent;
+        internal Node leftChild;
+        internal Node rightChild;
+        internal double inOrderSuccesor;
+        internal double inOrderPredecessor;
+
+        public Node(double key, double value)
+        {
+            _key = key;
+            _value = value;
+        }
+
+        internal void SwapKeyValuePair(double newKey, double newValue)
+        {
+            _key = newKey;
+            _value = newValue;
+        }
+
+        public override string ToString()
+        {
+            return Key + "," + Value;
+        }
+    }
+
+    public class TreeEnumerator : IEnumerator
+    {
+        private Node _currentNode = null;
+        private Node _nextNode = null;
+        private Node _rootNode;
+
+        public TreeEnumerator(Node root)
+        {
+            _rootNode = root;
+            _nextNode = SpectrumTree.GetMinNode(_rootNode);
+        }
+
+        public bool MoveNext()
+        {
+            if (_nextNode != null)
+            {
+                _currentNode = _nextNode;
+                _nextNode = SpectrumTree.GetInOrderSuccesor(_currentNode);
+                return true;
+            }
+
+            return false;
+        }
+
+        public void Reset()
+        {
+            _currentNode = null;
+            _nextNode = SpectrumTree.GetMinNode(_rootNode);
+        }
+
+        object IEnumerator.Current => _currentNode;
+        public Node Current => _currentNode;
+
     }
 }
