01ea912087fdddb8ecf20aa0ae56aca1ee62f487
Edwin Laboy <63374885+elaboy@users.noreply.github.com>
2023-01-13 13:49:09 -0600

    Added Space

--- mzLib/Chemistry/Constants.cs
+++ mzLib/Chemistry/Constants.cs
@@ -14,7 +14,7 @@
 // License for more details.
 //
 // You should have received a copy of the GNU Lesser General Public
-// License along with Chemistry Library. If not, see <http://www.gnu.org/licenses/>.
+// License  along with Chemistry Library. If not, see <http://www.gnu.org/licenses/>.
 
 namespace Chemistry
 {
