5b93eaad1999e10bf99ad8005a5353291e624339
avcarr2 <64652734+avcarr2@users.noreply.github.com>
2023-01-20 10:20:41 -0600

    Added infrastructure for MRS noise estimation

---
+++ mzLib/SpectralAveraging/MrsNoiseEstimation/ModWtOutput.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpectralAveraging;
+
+public class ModWtOutput
+{
+    public ModWtOutput(int maxScale, WaveletType waveletType, BoundaryType boundaryType)
+    {
+        Levels = new List<Level>();
+        MaxScale = maxScale;
+        WaveletType = waveletType;
+        BoundaryType = boundaryType;
+    }
+
+    public List<Level> Levels { get; private set; }
+    public int MaxScale { get; private set; }
+    public WaveletType WaveletType { get; }
+    public BoundaryType BoundaryType { get; }
+
+    public void AddLevel(double[] waveletCoeff, double[] scalingCoeff, int scale,
+        BoundaryType boundaryType, int originalSignalLength, int filterLength)
+    {
+        if (boundaryType == BoundaryType.Reflection)
+        {
+            int startIndex = ((int)Math.Pow(2, scale) - 1) * (filterLength - 1);
+            int stopIndex = Math.Min(startIndex + originalSignalLength, waveletCoeff.Length - 1);
+            Levels.Add(new Level(scale,
+                waveletCoeff[startIndex..stopIndex],
+                scalingCoeff[startIndex..stopIndex]));
+        }
+    }
+}
