f06cbc00e1b197f202b0cec86407b506cae9c7bf
nbollis <nbollis@comcast.net>
2023-01-31 11:04:53 -0600

    Started initial structure

---
+++ mzLib/MassSpectrometry/Deconvolution/Parameters/ThrashDeconvolutionParameters.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MassSpectrometry
+{
+    public class ThrashDeconvolutionParameters : DeconvolutionParameters
+    {
+        public double MinMsFeatureToBackgroundRation { get; set; }
+
+        public ThrashDeconvolutionParameters(double minMsFeatureToBackgroundRation = 3)
+        {
+            MinMsFeatureToBackgroundRation = minMsFeatureToBackgroundRation;
+        }
+    }
+}
