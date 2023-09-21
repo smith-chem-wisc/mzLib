b41708761e71dd8c5e371b1d1ffa44a6f1232328
Michael Shortreed <mshort@chem.wisc.edu>
2023-09-21 10:13:13 -0500

    snipper

--- mzLib/Test/FileReadingTests/TestMzML.cs
+++ mzLib/Test/FileReadingTests/TestMzML.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Security;
 using System.Threading.Tasks;
 using System.Xml.Serialization;
 using Chemistry;
@@ -94,6 +95,96 @@ namespace Test.FileReadingTests
 
             Assert.AreEqual(6, scans[0].MassSpectrum.XArray.Count());
             Assert.AreEqual(20, scans[1].MassSpectrum.XArray.Count());
+        }
+
+        [Test]
+        public static void SnipMzMl()
+        {
+            string origDataFile = @"E:\Projects\GlycoNickLei_PXD017646\2019_07_30_GlycoPepMix_35trig_EThcD35_rep2.raw";
+            int startScan = 5715;
+            int endScan = 5915;
+            FilteringParams filter = new FilteringParams(200, 0.01, 1, null, false, false, true);
+            var reader = MsDataFileReader.GetDataFile(origDataFile);
+            reader.LoadAllStaticData(filter, 1);
+
+            var scans = reader.GetAllScansList();
+            var scansToKeep = scans.Where(x => x.OneBasedScanNumber >= startScan && x.OneBasedScanNumber <= endScan).ToList();
+
+            List<(int oneBasedScanNumber, int? oneBasedPrecursorScanNumber)> scanNumbers = new List<(int oneBasedScanNumber, int? oneBasedPrecursorScanNumber)>();
+
+            foreach (var scan in scansToKeep)
+            {
+                if (scan.OneBasedPrecursorScanNumber.HasValue)
+                {
+                    scanNumbers.Add((scan.OneBasedScanNumber, scan.OneBasedPrecursorScanNumber.Value));
+                }
+                else
+                {
+                    scanNumbers.Add((scan.OneBasedScanNumber, null));
+                }
+            }
+
+            Dictionary<int, int> scanNumberMap = new Dictionary<int, int>();
+
+            foreach (var scanNumber in scanNumbers)
+            {
+                if (!scanNumberMap.ContainsKey(scanNumber.oneBasedScanNumber) && (scanNumber.oneBasedScanNumber - startScan + 1) > 0)
+                {
+                    scanNumberMap.Add(scanNumber.oneBasedScanNumber, scanNumber.oneBasedScanNumber - startScan + 1);
+                }
+                if (scanNumber.oneBasedPrecursorScanNumber.HasValue && !scanNumberMap.ContainsKey(scanNumber.oneBasedPrecursorScanNumber.Value) && (scanNumber.oneBasedPrecursorScanNumber.Value - startScan + 1) > 0)
+                {
+                    scanNumberMap.Add(scanNumber.oneBasedPrecursorScanNumber.Value, scanNumber.oneBasedPrecursorScanNumber.Value - startScan + 1);
+                }
+            }
+            List<MsDataScan> scansForTheNewFile = new List<MsDataScan>();
+
+
+            foreach (var scanNumber in scanNumbers)
+            {
+                MsDataScan scan = scansToKeep.First(x => x.OneBasedScanNumber == scanNumber.oneBasedScanNumber);
+
+                int? newOneBasedPrecursorScanNumber = null;
+                if (scan.OneBasedPrecursorScanNumber.HasValue && scanNumberMap.ContainsKey(scan.OneBasedPrecursorScanNumber.Value))
+                {
+                    newOneBasedPrecursorScanNumber = scanNumberMap[scan.OneBasedPrecursorScanNumber.Value];
+                }
+                MsDataScan newDataScan = new MsDataScan(
+                    scan.MassSpectrum,
+                    scanNumberMap[scan.OneBasedScanNumber],
+                    scan.MsnOrder,
+                    scan.IsCentroid,
+                    scan.Polarity,
+                    scan.RetentionTime,
+                    scan.ScanWindowRange,
+                    scan.ScanFilter,
+                    scan.MzAnalyzer,
+                    scan.TotalIonCurrent,
+                    scan.InjectionTime,
+                    scan.NoiseData,
+                    scan.NativeId.Replace(scan.OneBasedScanNumber.ToString(), scanNumberMap[scan.OneBasedScanNumber].ToString()),
+                    scan.SelectedIonMZ,
+                    scan.SelectedIonChargeStateGuess,
+                    scan.SelectedIonIntensity,
+                    scan.IsolationMz,
+                    scan.IsolationWidth,
+                    scan.DissociationType,
+                    newOneBasedPrecursorScanNumber,
+                    scan.SelectedIonMonoisotopicGuessMz,
+                    scan.HcdEnergy
+                );
+                scansForTheNewFile.Add(newDataScan);
+            }
+
+            string outPath = origDataFile.Replace(".raw", "_snip.mzML").ToString();
+
+            SourceFile sourceFile = new SourceFile(reader.SourceFile.NativeIdFormat,
+                reader.SourceFile.MassSpectrometerFileFormat, reader.SourceFile.CheckSum, reader.SourceFile.FileChecksumType, reader.SourceFile.Uri, reader.SourceFile.Id, reader.SourceFile.FileName);
+
+
+            MzmlMethods.CreateAndWriteMyMzmlWithCalibratedSpectra(new GenericMsDataFile(scansForTheNewFile.ToArray(), sourceFile), outPath, false);
+
+            Assert.IsTrue(false);
         }
 
         [Test]
