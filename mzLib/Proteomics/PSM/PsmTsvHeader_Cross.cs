d6d934b093a1169d406f000e740b19ef3fd256e9
Michael Shortreed <mshort@chem.wisc.edu>
2023-11-06 14:48:36 -0600

    psmFromTsv unit tests

--- mzLib/Proteomics/PSM/PsmTsvHeader_Cross.cs
+++ mzLib/Proteomics/PSM/PsmTsvHeader_Cross.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace Proteomics.PSM
 {
-    internal class PsmTsvHeader_Cross
+    public class PsmTsvHeader_Cross
     {
+        // Crosslinks
+        public const string CrossTypeLabel = "Cross Type";
+        public const string LinkResiduesLabel = "Link Residues";
+        public const string ProteinLinkSiteLabel = "Protein Link Site";
+        public const string RankLabel = "Rank";
+        public const string BetaPeptideProteinAccessionLabel = "Beta Peptide Protein Accession";
+        public const string BetaPeptideProteinLinkSiteLabel = "Beta Peptide Protein LinkSite";
+        public const string BetaPeptideBaseSequenceLabel = "Beta Peptide Base Sequence";
+        public const string BetaPeptideFullSequenceLabel = "Beta Peptide Full Sequence";
+        public const string BetaPeptideTheoreticalMassLabel = "Beta Peptide Theoretical Mass";
+        public const string BetaPeptideScoreLabel = "Beta Peptide Score";
+        public const string BetaPeptideRankLabel = "Beta Peptide Rank";
+        public const string BetaPeptideMatchedIonsLabel = "Beta Peptide Matched Ion Mass-To-Charge Ratios";
+        public const string XLTotalScoreLabel = "XL Total Score";
+        public const string ParentIonsLabel = "Parent Ions";
     }
 }
--- mzLib/Proteomics/PSM/PsmTsvHeader_Glyco.cs
+++ mzLib/Proteomics/PSM/PsmTsvHeader_Glyco.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace Proteomics.PSM
 {
-    internal class PsmTsvHeader_Glyco
+    public enum LocalizationLevel
     {
+        Level1,
+        Level1b,
+        Level2,
+        Level3
+    }
+    public class PsmTsvHeader_Glyco
+    {
+        public const string GlycanMass = "GlycanMass";
+        public const string GlycanComposition = "Plausible GlycanComposition";
+        public const string GlycanStructure = "Plausible GlycanStructure";
+        public const string GlycanLocalizationLevel = "GlycanLocalizationLevel";
+        public const string LocalizedGlycan = "Localized Glycans with Peptide Site Specific Probability";
     }
 }
