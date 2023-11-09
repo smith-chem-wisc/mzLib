8d88b3210645bf8951467040bd313338189a0137
Michael Shortreed <mshort@chem.wisc.edu>
2023-11-09 10:27:27 -0600

    someInterfaces

---
+++ mzLib/Omics/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Omics
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ mzLib/Omics/Fragmentation/IProduct.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Omics.Fragmentation
+{
+    public interface IProduct
+    {
+        double NeutralMass { get; }
+        ProductType ProductType { get; }
+        double NeutralLoss { get; }
+        FragmentationTerminus Terminus { get; }
+        int FragmentNumber { get; }
+        int Position { get; }
+        ProductType? SecondaryProductType { get; } //used for internal fragments
+        int SecondaryFragmentNumber { get; } //used for internal fragment ions
+        string Annotation { get; }
+
+    }
+}
