6631a89ca4d981249eefc205fc9afbace350359e
nbollis <nbollis@comcast.net>
2023-11-15 12:09:11 -0600

    Removed Extra using directives from SpectraFileAveraging

--- mzLib/SpectralAveraging/Algorithms/SpectraFileAveraging.cs
+++ mzLib/SpectralAveraging/Algorithms/SpectraFileAveraging.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using Easy.Common.Interfaces;
 using MassSpectrometry;
-using MathNet.Numerics.Statistics;
 using MzLibUtil;
 
 namespace SpectralAveraging;
