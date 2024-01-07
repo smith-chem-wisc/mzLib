db7296ef40408fe52a6d5dd899083063b91c4261
Alexander-Sol <AlexSolivais@gmail.com>
2024-01-07 15:42:45 -0600

    Started USI download stuff

---
+++ mzLib/Test/DatabaseTests/TestUsiDownload.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Test.DatabaseTests
+{
+    [TestFixture]
+    internal class TestUsiDownload
+    {
+        public const string Prefix = "mzspec";
+        public const string Identifier = "PXD000561";
+        public const string Run = "Adult_Frontalcortex_bRP_Elite_85_f09";
+        public const string TypeFlag = "scan";
+        public const string Index = "17555";
+        public const string Interpretation = "VLHPLEGAVVIIFK/2";
+
+        [Test]
+        public static void TestTryGetSpectra()
+        {
+            string usi = string.Join(':', new List<string> { Prefix, Identifier, Run, TypeFlag, Index });
+            Assert.True(UsiLoader.TryGetSpectrum(usi, out var spectrum));
+            Assert.NotNull(spectrum);
+        }
+
+        [Test]
+        public static void TestTryGetSpectraWithResults()
+        {
+            string usi = string.Join(':', new List<string> { Prefix, Identifier, Run, TypeFlag, Index, Interpretation });
+            Assert.True(UsiLoader.TryGetSpectrum(usi, out var spectrum, out var results));
+            Assert.NotNull(spectrum);
+            Assert.NotNull(results);
+        }
+    }
+}
---
+++ mzLib/UsefulProteomicsDatabases/UsiLoader.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsefulProteomicsDatabases
+{
+    public static class UsiLoader
+    {
+        public static bool TryGetSpectrum(string usi, out Object spectrum, UsiDatabase database = UsiDatabase.PRIDE)
+        {
+            string[] usiSplit = usi.Split(':');
+            if (usiSplit.Length < 5 | usiSplit.Length > 6)
+                throw new ArgumentException("USI string was improperly formatted");
+
+            string baseUrl = GetRequestUrl(database);
+            string apiQuery = baseUrl + "usi=" + string.Join('&', usiSplit);
+
+            var apiResponse = Loaders.AwaitAsync_GetSomeData(apiQuery);
+
+            spectrum = null;
+            return false;
+        }
+
+        
+
+        internal static string GetRequestUrl(UsiDatabase db)
+        {
+            switch (db)
+            {
+                case (UsiDatabase.PRIDE):
+                    return "https://www.ebi.ac.uk/pride/ws/archive/v2/spectra?";
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+    }
+
+    public enum UsiDatabase
+    {
+        PRIDE,
+        Unknown
+    }
+}
