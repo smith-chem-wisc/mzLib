fb28fbe9606fb4069100a433c9472ed3f4e7e15e
nbollis <nbollis@comcast.net>
2024-01-18 18:27:37 -0600

    Added nucleotide and Rnase

---
+++ mzLib/Transcriptomics/Digestion/Rnase.cs
@@ -0,0 +1,52 @@
+﻿using Chemistry;
+using Omics.Digestion;
+using Omics.Modifications;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Transcriptomics.Digestion
+{
+    public class Rnase : DigestionAgent, IEquatable<Rnase>
+    {
+        public Rnase(string name, CleavageSpecificity cleaveSpecificity, List<DigestionMotif> motifList, Modification cleavageMod = null) :
+            base(name, cleaveSpecificity, motifList, cleavageMod)
+        {
+            Name = name;
+            CleavageSpecificity = cleaveSpecificity;
+            DigestionMotifs = motifList;
+        }
+
+        // TODO: Coming soon to a mzLib near you
+        // public List<NucleolyticOligo> GetUnmodifiedOligos(NucleicAcid nucleicAcid, int maxMissedCleavages, int minLength, int maxLength)
+        // private IEnumerable<NucleolyticOligo> FullDigestion(NucleicAcid nucleicAcid, int maxMissedCleavages, int minLength, int maxLength)
+        
+        public bool Equals(Rnase? other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return Name == other.Name;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Rnase)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
