e4a58664ba765f7636cf223e6977d16d493fcaa5
nbollis <nbollis@comcast.net>
2024-01-22 12:18:13 -0600

    Fixed unit tests by adjusting minLength in digestion params

--- mzLib/Transcriptomics/Digestion/RnaDigestionParams.cs
+++ mzLib/Transcriptomics/Digestion/RnaDigestionParams.cs
@@ -5,7 +5,7 @@ namespace Transcriptomics.Digestion
 {
     public class RnaDigestionParams : IDigestionParams
     {
-        public RnaDigestionParams(string rnase = "top-down", int maxMissedCleavages = 0, int minLength = 6, 
+        public RnaDigestionParams(string rnase = "top-down", int maxMissedCleavages = 0, int minLength = 3, 
             int maxLength = int.MaxValue, int maxModificationIsoforms = 1024, int maxMods = 2,
             FragmentationTerminus fragmentationTerminus = FragmentationTerminus.Both) 
         {
