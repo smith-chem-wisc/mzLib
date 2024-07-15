1da1161e12e0be6e27d5a3f673d46d8d80ef35d4
RayMSMS <cyang482@wisc.edu>
2024-07-15 14:44:33 -0500

    update 7/15/2024 1. Create the new class "Extremum" to store the local extre number 2. More XIC method, BuildSmoothedCubicSpline and "FindExtrema"

--- mzLib/FlashLFQ/Alex project/XIC.cs
+++ mzLib/FlashLFQ/Alex project/XIC.cs
@@ -9,6 +9,9 @@ using MathNet.Numerics.IntegralTransforms;
 using System.Runtime.CompilerServices;
 using System.Numerics;
 using Omics.Fragmentation;
+using Readers.Generated;
+using System.Threading;
+using Easy.Common.Extensions;
 
 namespace FlashLFQ.Alex_project
 {
@@ -19,7 +22,9 @@ namespace FlashLFQ.Alex_project
         public  SpectraFileInfo SpectraFile { get; init; }
         public  bool Reference { get; init; }
         public MathNet.Numerics.Interpolation.LinearSpline LinearSpline { get; private set;}
+        public CubicSpline SmoothedCubicSpline { get; private set; }
         public double RtShift { get; private set; }
+        List<Extremum> Extrema { get; set; }
 
         public XIC(List<IndexedMassSpectralPeak> peaks, double peakFindingMass, SpectraFileInfo spectraFile)
         {
@@ -125,6 +130,74 @@ namespace FlashLFQ.Alex_project
             return rtShift;
 
             // Example
+        }
+
+        /// <summary>
+        /// Try to smooth the XIC by averaging the intensity of the points
+        /// </summary>
+        /// <param name="pointsToAverage"> should be odds number. The number of points to average for smoothing the XIC </param>
+        /// <exception cref="ArgumentException"></exception>
+        public void BuildSmoothedCubicSpline(int pointsToAverage = 3)
+        {
+            if (pointsToAverage <= 0)
+            {
+                throw new ArgumentException("pointsToAverage must be greater than 0");
+            }
+            double[] intensity = Ms1Peaks.Select(p => p.Intensity).ToArray();
+            double[] retentionTime = Ms1Peaks.Select(p => p.RetentionTime + RtShift).ToArray();
+            double[] smoothedIntensity = new double[intensity.Length];
+
+            for (int i = 0; i < intensity.Length; i++) //smooth the intensity
+            {
+                if (i < pointsToAverage / 2)
+                {
+                    smoothedIntensity[i] = 0;
+                }
+
+                else if (i >= intensity.Length - pointsToAverage / 2)
+                {
+                    smoothedIntensity[i] = 0;
+                }
+
+                else
+                {
+                    double sum = 0;
+                    for (int j = 0; j < pointsToAverage; j++)
+                    {
+                        sum += intensity[i + j - pointsToAverage / 2];
+                    }
+                    smoothedIntensity[i] = sum / pointsToAverage;
+                }
+               
+            }
+
+            this.SmoothedCubicSpline = CubicSpline.InterpolateAkima(retentionTime, smoothedIntensity);
+        }
+
+
+        /// <summary>
+        /// Use the second derivative to find the local maxmun and minimum points
+        /// </summary>
+        public void FindExtrema()
+        {
+            Extrema = new List<Extremum>();
+            double[] extrePoints = SmoothedCubicSpline.StationaryPoints(); //extrePoint is the retentionTime for the point's first derivative is zero
+            foreach (var point in extrePoints) 
+            {
+                if (SmoothedCubicSpline.Differentiate2(point) > 0) //Local Maxmun point
+                {
+                    Extrema.Add(new Extremum(Ms1Peaks.Where(p => p.RetentionTime == point).First().Intensity, point, ExtremumType.Maximum));
+                    Extrema.Sort();
+                }
+                if (SmoothedCubicSpline.Differentiate2(point) > 0) //Local Minmun point
+                {
+                    Extrema.Add(new Extremum(Ms1Peaks.Where(p => p.RetentionTime == point).First().Intensity, point, ExtremumType.Minimum));
+                    Extrema.Sort();
+                }
+                
+            }
+
+
         }
 
     }
