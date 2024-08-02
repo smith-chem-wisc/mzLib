6720fc54d75a2645944689548cb40fbf2c2f4116
nbollis <nbollis@comcast.net>
2024-08-02 14:28:51 -0500

    instantiated johhnydeconparams.decontype

--- mzLib/MassSpectrometry/Deconvolution/Parameters/JohnnyDeconvolutionParameters.cs
+++ mzLib/MassSpectrometry/Deconvolution/Parameters/JohnnyDeconvolutionParameters.cs
@@ -8,7 +8,8 @@ namespace MassSpectrometry
 {
     public class JohnnyDeconvolutionParameters : DeconvolutionParameters
     {
-        public override DeconvolutionType DeconvolutionType { get; protected set; }
+        public override DeconvolutionType DeconvolutionType { get; protected set; } =
+            DeconvolutionType.JohnnyDeconvolution;
 
         public JohnnyDeconvolutionParameters(Polarity polarity = Polarity.Positive) 
             : base(1, 100, polarity)
