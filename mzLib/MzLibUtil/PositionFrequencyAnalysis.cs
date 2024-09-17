d2c240e043c201231836e76bf3951d8c28ddd0af
Peter Cruz Parrilla <peter.cruzparrilla@gmail.com>
2024-09-17 15:45:16 -0500

    temp

---
+++ mzLib/MzLibUtil/PositionFrequencyAnalysis.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using FlashLFQ;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MzLibUtil
+{
+    public class PositionFrequencyAnalyses
+    {
+        public static Dictionary<int, Dictionary<string, double>> PTMOccupancy(List<FlashLFQ.Peptide.Peptide> peptides)
+        {
+            
+            Dictionary<int, Dictionary<string, double>> occupancy;
+            foreach (var peptide in peptides.Sort(p => p.sequence))
+            {
+                peptide.
+            }
+        }
+    }
+}
