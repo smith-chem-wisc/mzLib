40605ec294999cfabb930e1f077d6edc06d221a3
nbollis <nbollis@comcast.net>
2024-10-08 16:46:28 -0500

    Added Readers.Util support for MsAlign files

--- mzLib/Readers/Util/Software.cs
+++ mzLib/Readers/Util/Software.cs
@@ -4,14 +4,23 @@
     {
         Unspecified,
         MassSpecFile,
+
+        // Deconvolution Results
         FLASHDeconv, // files tested were outputted from OpenMs3.0.0
         TopFD,       // files tested were outputted from v1.6.2
+
+        // Search Results
         MetaMorpheus,
         MaxQuant,
         Toppic,
         MsFragger, // files tested were from fragpipe v21.1
-        FlashLFQ,
         MsPathFinderT,
-        Crux
+        Crux,
+        ProteomeDiscoverer,
+        ProsightPD,
+        Chimerys,
+
+        // Quantification Results
+        FlashLFQ,
     }
 }
