c33324dfb3a9f9d1e37c1c2aab6fec217f02f7cd
Nic Bollis <nbollis@wisc.edu>
2024-10-11 00:47:44 -0500

    Finish NeutralMassSpectrum
    - Added `InternalsVisibleTo` entries for "Development" and "Test" in `MassSpectrometry.csproj`.
    - Changed `MostAbundantObservedIsotopicMass` to `internal` in `IsotopicEnvelope.cs`.
    - Added a new constructor to `IsotopicEnvelope` with monoisotopic mass, intensity, and charge.
    - Added XML documentation and changed `GeneratePeak` to `protected virtual` in `MzSpectrum.cs`.
    - Removed unused `using` directives in `MzSpectrum.cs` and `NeutralMzSpectrum.cs`.
    - Modified `NeutralMzSpectrum` constructor to validate array lengths.
    - Added `Charges` property to `NeutralMzSpectrum` and initialized it in the constructor.
    - Overrode `GeneratePeak` in `NeutralMzSpectrum` to convert to a charged spectrum using `Charges`.

---
+++ mzLib/MassSpectrometry/MzSpectra/NeutralMassSpectrum.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Chemistry;
+
+namespace MassSpectrometry.MzSpectra
+{
+    public class NeutralMzSpectrum : MzSpectrum
+    {
+        public int[] Charges { get; init; }
+        public NeutralMzSpectrum(double[,] monoisotopicMassesIntensities, int[] charges) : base(monoisotopicMassesIntensities)
+        {
+            if (monoisotopicMassesIntensities.Length != charges.Length || monoisotopicMassesIntensities.Length != monoisotopicMassesIntensities.Rank)
+                throw new ArgumentException("The lengths of monoisotopicMasses, intensities, and charges must be the same.");
+
+            Charges = charges;
+        }
+
+        public NeutralMzSpectrum(double[] monoisotopicMasses, double[] intensities, int[] charges, bool shouldCopy)
+            : base(monoisotopicMasses, intensities, shouldCopy)
+        {
+            if (monoisotopicMasses.Length != intensities.Length || monoisotopicMasses.Length != charges.Length)
+                throw new ArgumentException("The lengths of monoisotopicMasses, intensities, and charges must be the same.");
+
+            Charges = charges;
+        }
+
+        /// <summary>
+        /// Converts to a charged spectrum
+        /// </summary>
+        protected override MzPeak GeneratePeak(int index)
+        {
+            return new MzPeak(XArray[index].ToMz(Charges[index]), YArray[index]);
+        }
+    }
+}
