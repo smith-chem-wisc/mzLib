f6caa30afc7d71dc3612a4f3dbc046514b1b37ce
Peter Cruz Parrilla <peter.cruzparrilla@gmail.com>
2024-10-18 13:37:41 -0500

    Refactored the PositionFrequencyAnalysis code to eliminate the nested dictionaries using some data objects instead for code readability. Updated all of the previous tests (MzLibUtil and FlashLFQ) to accomodate for the refactoring.

--- mzLib/MzLibUtil/PositionFrequencyAnalysis.cs
+++ mzLib/MzLibUtil/PositionFrequencyAnalysis.cs
@@ -1,13 +1,81 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 using Easy.Common.Extensions;
 
 namespace MzLibUtil
 {
+    // Should this have all of the parent data (i.e. protein group, protein, peptide, peptide position)? Unnecessary for now, but probably useful later.
+    public class UtilModification
+    {
+        public string ModName { get; set; }
+        public int Position { get; set; }
+        public double Intensity { get; set; }
+
+        public UtilModification(string name, int position, double intensity)
+        {
+            ModName = name;
+            Position = position;
+            Intensity = intensity;
+        }
+
+    }
+    public class UtilPeptide
+    {
+        public string FullSequence { get; set; }
+        public string BaseSequence { get; set; }
+        public UtilProtein ParentProtein { get; set; }
+        public int IndexInProtein { get; set; }
+        public Dictionary<int, Dictionary<string, UtilModification>> ModifiedAminoAcidPositions { get; set; }
+        public double Intensity { get; set; } 
+
+        public UtilPeptide(string fullSeq, Dictionary<int, Dictionary<string, UtilModification>> mods = null) 
+        {
+            FullSequence = fullSeq;
+            ModifiedAminoAcidPositions = mods.IsNotNullOrEmpty() ? mods : new Dictionary<int, Dictionary<string, UtilModification>>();
+            SetBaseSequence();
+        }
+        public void SetBaseSequence(string modPattern = @"\[.*?\]")
+        {
+            Regex regexSpecialChar = new(modPattern);
+            BaseSequence = regexSpecialChar.Replace(FullSequence, @"");
+        }
+        public void AddModifications(Dictionary<int, string> mods)
+        {
+            throw new NotImplementedException();
+        }
+        public void PeptideToProteinPositions()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    
+    public class UtilProtein
+    {
+        public string Name { get; set; }
+        public string Sequence { get; set; }
+        public Dictionary<string, UtilPeptide> Peptides { get; set; }
+
+        public UtilProtein(string name, Dictionary<string, UtilPeptide> peptides=null)
+        {
+            Name = name;
+            if (peptides != null) Peptides = peptides;
+            else Peptides= new Dictionary<string, UtilPeptide>();
+        }
+    }
+
+    public class UtilProteinGroup
+    {
+        public string Name { get; set;}
+        public Dictionary<string, UtilProtein> Proteins {  get; set; }
+
+        public UtilProteinGroup(string name, Dictionary<string, UtilProtein> proteins = null)
+        {
+            Name = name;
+            if (proteins != null) Proteins = proteins;
+            else Proteins= new Dictionary<string, UtilProtein>();
+        }
+    }
     public class PositionFrequencyAnalysis
     {
         // TODO: Write PeptideToProteinPTMOccupancy which takes a dictionary of ProteinAccession:ProteinSequence for determining index offsets to the peptide modification indices. 
@@ -15,82 +83,84 @@ namespace MzLibUtil
         /// <summary>
         /// Calculates the occupancy of post-translational modifications at the peptide level. 
         /// </summary>
-        /// <param name="peptides"> A List of Tuples whose entries are ordered as (string FullSequence, string BaseSequence, HashSet<string> ProteinGroups, Intensity) for each peptide.</param>
+        /// <param name="peptides"> A List of Tuples whose entries are ordered as (string FullSequence, string BaseSequence, List<string> ProteinGroups, Intensity) for each peptide.</param>
         /// <param name="IncludeNTerminus"> If true, the index of modifications at the N-terminus will be 0 (zero-based indexing). Otherwise, it is the index of the first amino acid (one-based indexing).</param>
         /// <param name="IncludeCTerminus"> If true, the index of modifications at the C-terminus will be one more than the index of the last amino acid. Otherwise, it is the index of the last amino acid.</param>
         /// <returns> A nested dictionary whose key mappings are as follows: string ProteinGroup-> string Protein-> string BaseSequence-> int ModifiedAminoAcidIndex-> string ModificationName-> double Intensity
         /// Note: Each BaseSequence dictionary contains a ModifiedAminoAcidIndex key of -1 that then contains a ModificationName key called "Total" that is used to track the total intensity observed for 
         /// all of the amino acids in that peptide.</returns>
-        public static Dictionary<string, Dictionary<string, Dictionary<string, Dictionary<int, Dictionary<string, double>>>>> PeptidePTMOccupancy(List<Tuple<string, string, List<string>, double>> peptides, bool IncludeNTerminus=true, bool IncludeCTerminus=true)
+        public static Dictionary<string, UtilProteinGroup> PeptidePTMOccupancy(List<Tuple<string, string, List<string>, double>> peptides, bool IncludeNTerminus = true, bool IncludeCTerminus = true)
         {
-            var occupancy = new Dictionary<string, Dictionary<string, Dictionary<string, Dictionary<int, Dictionary<string, double>>>>>();
+            var proteinGroups = new Dictionary<string, UtilProteinGroup>();
             
             // Go through the peptides given
-            foreach (var peptide in peptides)
+            foreach (var pep in peptides)
             {
-                double peptideIntensity = peptide.Item4;
-                
-                string baseSeq;
-                if (!peptide.Item2.IsNotNullOrEmpty())
-                {
-                    baseSeq = new string(peptide.Item1.ToCharArray());
-                    ClassExtensions.RemoveSpecialCharacters(ref baseSeq, @"", @"[.*?]");
-                }
-                else { baseSeq = peptide.Item2; }
+                string fullSeq = pep.Item1;
+                string baseSeq = pep.Item2.IsNotNullOrEmpty() ? pep.Item2 : new string(pep.Item1.ToCharArray());
+                ClassExtensions.RemoveSpecialCharacters(ref baseSeq, @"", @"[.*?]"); // in case it is null or empty and we need to get the base sequence from the full sequence
+                List<string> pgs = pep.Item3;
+                double peptideIntensity = pep.Item4;
 
                 // Go through the peptide's protein groups
-                foreach(var pg in peptide.Item3)
+                foreach (var pgName in pgs)
                 {
                     // If have not seen that protein group, store it
-                    if (!occupancy.ContainsKey(pg))
+                    if (!proteinGroups.ContainsKey(pgName))
                     {
-                        occupancy.Add(pg, new Dictionary<string, Dictionary<string, Dictionary<int, Dictionary<string, double>>>>());
+                        proteinGroups[pgName] = new UtilProteinGroup(pgName);
                     }
+                    var proteinGroup = proteinGroups[pgName];
 
                     // Go through the proteins in each protein group
-                    foreach (var protein in pg.Split('|'))
+                    foreach (var proteinName in pgName.Split('|'))
                     {
                         // Add the protein to the protein group's dictionary if it has not been added
-                        if (!occupancy[pg].ContainsKey(protein))
+                        if (!proteinGroup.Proteins.ContainsKey(proteinName))
                         {
-                            occupancy[pg].Add(protein, new Dictionary<string, Dictionary<int, Dictionary<string, double>>>());
+                            proteinGroup.Proteins[proteinName] = new UtilProtein(proteinName);
                         }
+                        var protein = proteinGroup.Proteins[proteinName];
+
                         // If the peptide's base sequence has not been seen, add it to the protein's dictionary
-                        if (!occupancy[pg][protein].ContainsKey(baseSeq))
+                        if (!protein.Peptides.ContainsKey(baseSeq))
                         {
-                            occupancy[pg][protein].Add(baseSeq, new Dictionary<int, Dictionary<string, double>>());
-                            occupancy[pg][protein][baseSeq].Add(-1, new Dictionary<string, double>());
-                            occupancy[pg][protein][baseSeq][-1].Add("Total", 0);
+                            protein.Peptides[baseSeq] = new UtilPeptide(fullSeq);
+                            protein.Peptides[baseSeq].Intensity = 0;
                         }
+
                         // Increase the total intensity of the peptide base sequence to track the total intensity of all amino acids in that sequence
-                        occupancy[pg][protein][baseSeq][-1]["Total"] += peptideIntensity;
+                        protein.Peptides[baseSeq].Intensity += peptideIntensity;
+                        var peptide = protein.Peptides[baseSeq];
 
                         // Want both arguments passed here to be true if need to later filter out peptide terminal mods that are not protein terminal mods 
-                        Dictionary<int, List<string>> peptideMods = peptide.Item1.ParseModifications(IncludeNTerminus, IncludeCTerminus); 
+                        Dictionary<int, List<string>> peptideMods = fullSeq.ParseModifications(IncludeNTerminus, IncludeCTerminus);
                         // Go through the modified positions found froum the full sequence
                         foreach (var modpos in peptideMods)
                         {
                             // If that position has not been recorded as containing a modification, add it to the base sequence's dictonary
-                            if (!occupancy[pg][protein][baseSeq].ContainsKey(modpos.Key))
+                            if (!peptide.ModifiedAminoAcidPositions.ContainsKey(modpos.Key))
                             {
-                                occupancy[pg][protein][baseSeq].Add(modpos.Key, new Dictionary<string, double>());
+                                peptide.ModifiedAminoAcidPositions[modpos.Key] = new Dictionary<string, UtilModification>();
                             }
+                            var modifiedPosition = peptide.ModifiedAminoAcidPositions[modpos.Key];
+
                             // Go through the modifications found at a modified amino acid index
                             foreach (var mod in modpos.Value)
                             {
                                 //If the name of that modification has not been seen, record that modification in the index's dictionary with an intensity of 0
-                                if (!occupancy[pg][protein][baseSeq][modpos.Key].ContainsKey(mod))
+                                if (!modifiedPosition.ContainsKey(mod))
                                 {
-                                    occupancy[pg][protein][baseSeq][modpos.Key].Add(mod, 0);
+                                    modifiedPosition[mod] = new UtilModification(mod, modpos.Key, 0);
                                 }
                                 // Increase the intensity of the modification by the intensity of the peptide
-                                occupancy[pg][protein][baseSeq][modpos.Key][mod] += peptideIntensity;
+                                modifiedPosition[mod].Intensity += peptideIntensity;
                             }
                         }
                     }
                 }
             }
-            return occupancy;
+            return proteinGroups;
         }
     }
 }
