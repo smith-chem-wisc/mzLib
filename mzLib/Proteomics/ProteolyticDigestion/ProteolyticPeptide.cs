1b8b95024604b80cf3c01895c994f097b55b69af
Nic Bollis <nbollis@comcast.net>
2025-01-16 18:49:43 -0600

    Digestion Consolidation and Optimization (#823)
    * Added bassic object pools
    
    * Refactor DigestionAgent to use HashSetPool for indices
    
    Added `using` directive for `MzLibUtil`. Introduced a static readonly `HashSetPool<int>` named `HashSetPool` to manage a pool of hash sets. Updated `DigestionAgent` constructor to initialize `HashSetPool`. Refactored `GetDigestionSiteIndices` to use a hash set from `HashSetPool` for storing indices, ensuring no duplicates. Explicitly added start and end of protein sequence as cleavage sites. Implemented `try-finally` block to return hash set to pool after use. Final list of indices is now sorted before returning.
    
    * string interpolation in BPWSM extensions
    
    * Adjusted IEnumerable return in Protease.GetUnmodified
    
    * Digestion Optimizations
    
    * Moved testing class to proper subdirectory
    
    * Adjusted ModFits to have the correct localization for peptide and protein termini
    
    * Cleaned up hashset return
    
    * Digestion Agent Hashset Return Cleanup
    
    * set fixed mods now modifies in place using a pooled dictionary
    
    * Added comments to digeston
    
    * Refactor code for readability and efficiency
    
    - Simplified initial check for `possibleVariableModifications.Count` and replaced `yield return null` with `yield break`.
    - Adjusted indentation and loop structure for clarity.
    - Refactored nested loop to remove unnecessary braces and streamline logic.
    - Simplified construction of `modificationPattern` dictionary by removing redundant checks and directly using `modIndex`.
    
    * Added many comments
    
    * set fixed mods namechange
    
    * Eliminated IsN or IS5' in favor of unified method
    
    * Extracted all variable modification combination generation to parent class
    
    * removed fixed mods changes
    
    * removed unnecessary namespace
    
    * Extracted AppendFixedToVariabel
    
    * Update mzLib.nuspec
    
    * Added shortreed comment

--- mzLib/Proteomics/ProteolyticDigestion/ProteolyticPeptide.cs
+++ mzLib/Proteomics/ProteolyticDigestion/ProteolyticPeptide.cs
@@ -1,7 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Security.Cryptography;
 using Omics.Digestion;
 using Omics.Modifications;
 
@@ -14,7 +12,6 @@ namespace Proteomics.ProteolyticDigestion
     [Serializable]
     public class ProteolyticPeptide : DigestionProduct
     {
-        
         internal ProteolyticPeptide(Protein protein, int oneBasedStartResidueInProtein, int oneBasedEndResidueInProtein, int missedCleavages, CleavageSpecificity cleavageSpecificityForFdrCategory, string peptideDescription = null, string baseSequence = null) :
             base(protein, oneBasedStartResidueInProtein, oneBasedEndResidueInProtein, missedCleavages, cleavageSpecificityForFdrCategory, peptideDescription, baseSequence)
         {
@@ -51,142 +48,40 @@ namespace Proteomics.ProteolyticDigestion
         /// <param name="digestionParams"></param>
         /// <param name="variableModifications"></param>
         /// <returns></returns>
-        internal IEnumerable<PeptideWithSetModifications> GetModifiedPeptides(IEnumerable<Modification> allKnownFixedModifications,
+        internal IEnumerable<PeptideWithSetModifications> GetModifiedPeptides(List<Modification> allKnownFixedModifications,
             DigestionParams digestionParams, List<Modification> variableModifications)
         {
+            int variable_modification_isoforms = 0;
             int peptideLength = OneBasedEndResidue - OneBasedStartResidue + 1;
             int maximumVariableModificationIsoforms = digestionParams.MaxModificationIsoforms;
             int maxModsForPeptide = digestionParams.MaxModsForPeptide;
-            var twoBasedPossibleVariableAndLocalizeableModifications = new Dictionary<int, List<Modification>>(peptideLength + 4);
+            var twoBasedPossibleVariableAndLocalizeableModifications = DictionaryPool.Get();
+            var fixedModDictionary = FixedModDictionaryPool.Get();
 
-            var pepNTermVariableMods = new List<Modification>();
-            twoBasedPossibleVariableAndLocalizeableModifications.Add(1, pepNTermVariableMods);
-
-            var pepCTermVariableMods = new List<Modification>();
-            twoBasedPossibleVariableAndLocalizeableModifications.Add(peptideLength + 2, pepCTermVariableMods);
-
-            foreach (Modification variableModification in variableModifications)
+            try
             {
-                // Check if can be a n-term mod
-                if (CanBeNTerminalMod(variableModification, peptideLength) && !ModificationLocalization.UniprotModExists(Protein, 1, variableModification))
-                {
-                    pepNTermVariableMods.Add(variableModification);
-                }
+                PopulateVariableModifications(variableModifications, in twoBasedPossibleVariableAndLocalizeableModifications);
+                PopulateFixedModsOneIsNorFivePrimeTerminus(peptideLength, allKnownFixedModifications, in fixedModDictionary);
 
-                for (int r = 0; r < peptideLength; r++)
+                foreach (Dictionary<int, Modification> variableModPattern in GetVariableModificationPatterns(twoBasedPossibleVariableAndLocalizeableModifications, maxModsForPeptide, peptideLength))
                 {
-                    if (ModificationLocalization.ModFits(variableModification, Protein.BaseSequence, r + 1, peptideLength, OneBasedStartResidue + r)
-                        && variableModification.LocationRestriction == "Anywhere." && !ModificationLocalization.UniprotModExists(Protein, r + 1, variableModification))
+                    AppendFixedModificationsToVariable(in fixedModDictionary, in variableModPattern, out int numFixedMods);
+
+                    yield return new PeptideWithSetModifications(Protein, digestionParams, OneBasedStartResidue, OneBasedEndResidue,
+                        CleavageSpecificityForFdrCategory, PeptideDescription, MissedCleavages, variableModPattern, numFixedMods);
+
+                    variable_modification_isoforms++;
+                    if (variable_modification_isoforms == maximumVariableModificationIsoforms)
                     {
-                        if (!twoBasedPossibleVariableAndLocalizeableModifications.TryGetValue(r + 2, out List<Modification> residueVariableMods))
-                        {
-                            residueVariableMods = new List<Modification> { variableModification };
-                            twoBasedPossibleVariableAndLocalizeableModifications.Add(r + 2, residueVariableMods);
-                        }
-                        else
-                        {
-                            residueVariableMods.Add(variableModification);
-                        }
-                    }
-                }
-                // Check if can be a c-term mod
-                if (CanBeCTerminalMod(variableModification, peptideLength) && !ModificationLocalization.UniprotModExists(Protein, peptideLength, variableModification))
-                {
-                    pepCTermVariableMods.Add(variableModification);
-                }
-            }
-
-            // LOCALIZED MODS
-            foreach (var kvp in Protein.OneBasedPossibleLocalizedModifications)
-            {
-                bool inBounds = kvp.Key >= OneBasedStartResidue && kvp.Key <= OneBasedEndResidue;
-                if (!inBounds)
-                {
-                    continue;
-                }
-
-                int locInPeptide = kvp.Key - OneBasedStartResidueInProtein + 1;
-                foreach (Modification modWithMass in kvp.Value)
-                {
-                    if (modWithMass is Modification variableModification)
-                    {
-                        // Check if can be a n-term mod
-                        if (locInPeptide == 1 && CanBeNTerminalMod(variableModification, peptideLength) && !Protein.IsDecoy)
-                        {
-                            pepNTermVariableMods.Add(variableModification);
-                        }
-
-                        int r = locInPeptide - 1;
-                        if (r >= 0 && r < peptideLength
-                            && (Protein.IsDecoy ||
-                            (ModificationLocalization.ModFits(variableModification, Protein.BaseSequence, r + 1, peptideLength, OneBasedStartResidueInProtein + r)
-                                && variableModification.LocationRestriction == "Anywhere.")))
-                        {
-                            if (!twoBasedPossibleVariableAndLocalizeableModifications.TryGetValue(r + 2, out List<Modification> residueVariableMods))
-                            {
-                                residueVariableMods = new List<Modification> { variableModification };
-                                twoBasedPossibleVariableAndLocalizeableModifications.Add(r + 2, residueVariableMods);
-                            }
-                            else
-                            {
-                                residueVariableMods.Add(variableModification);
-                            }
-                        }
-
-                        // Check if can be a c-term mod
-                        if (locInPeptide == peptideLength && CanBeCTerminalMod(variableModification, peptideLength) && !Protein.IsDecoy)
-                        {
-                            pepCTermVariableMods.Add(variableModification);
-                        }
+                        yield break;
                     }
                 }
             }
-
-            int variable_modification_isoforms = 0;
-
-            foreach (Dictionary<int, Modification> kvp in GetVariableModificationPatterns(twoBasedPossibleVariableAndLocalizeableModifications, maxModsForPeptide, peptideLength))
+            finally
             {
-                int numFixedMods = 0;
-                foreach (var ok in GetFixedModsOneIsNorFivePrimeTerminus(peptideLength, allKnownFixedModifications))
-                {
-                    if (!kvp.ContainsKey(ok.Key))
-                    {
-                        numFixedMods++;
-                        kvp.Add(ok.Key, ok.Value);
-                    }
-                }
-                yield return new PeptideWithSetModifications(Protein, digestionParams, OneBasedStartResidue, OneBasedEndResidue,
-                    CleavageSpecificityForFdrCategory, PeptideDescription, MissedCleavages, kvp, numFixedMods);
-                variable_modification_isoforms++;
-                if (variable_modification_isoforms == maximumVariableModificationIsoforms)
-                {
-                    yield break;
-                }
+                FixedModDictionaryPool.Return(fixedModDictionary);
+                DictionaryPool.Return(twoBasedPossibleVariableAndLocalizeableModifications);
             }
-        }
-
-        /// <summary>
-        /// Determines whether given modification can be an N-terminal modification
-        /// </summary>
-        /// <param name="variableModification"></param>
-        /// <param name="peptideLength"></param>
-        /// <returns></returns>
-        private bool CanBeNTerminalMod(Modification variableModification, int peptideLength)
-        {
-            return ModificationLocalization.ModFits(variableModification, Protein.BaseSequence, 1, peptideLength, OneBasedStartResidue)
-                && (variableModification.LocationRestriction == "N-terminal." || variableModification.LocationRestriction == "Peptide N-terminal.");
-        }
-
-        /// <summary>
-        /// Determines whether given modification can be a C-terminal modification
-        /// </summary>
-        /// <param name="variableModification"></param>
-        /// <param name="peptideLength"></param>
-        /// <returns></returns>
-        private bool CanBeCTerminalMod(Modification variableModification, int peptideLength)
-        {
-            return ModificationLocalization.ModFits(variableModification, Protein.BaseSequence, peptideLength, peptideLength, OneBasedStartResidue + peptideLength - 1)
-                && (variableModification.LocationRestriction == "C-terminal." || variableModification.LocationRestriction == "Peptide C-terminal.");
         }
     }
 }
