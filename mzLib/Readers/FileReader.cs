4733d269cfe90f71d681eea3014ea2fbf203226f
RayMSMS <150720362+RayMSMS@users.noreply.github.com>
2025-02-04 11:49:25 -0600

    sapce

--- mzLib/Readers/FileReader.cs
+++ mzLib/Readers/FileReader.cs
@@ -4,6 +4,7 @@
     {
         public static TResultFile ReadFile<TResultFile>(string filePath) where TResultFile : IResultFile, new()
         {
+            
             var resultFile = new TResultFile() { FilePath = filePath };
             resultFile.LoadResults();
             return resultFile;
