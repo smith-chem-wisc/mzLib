156a2bfbbf7ce39c282a8a1fd083399d1d2c722a
trishorts <mshort@chem.wisc.edu>
2025-03-12 13:24:23 -0500

    add spectrum libraries folder to reader and MspSpectrumLibraryReader placeholder

---
+++ mzLib/Readers/SpectrumLibraries/LibrarySpectrumFile.cs
@@ -0,0 +1,28 @@
+﻿using Omics.SpectrumMatch;
+
+namespace Readers.SpectrumLibraries
+{
+    public class LibrarySpectrumFile : ResultFile<LibrarySpectrum>, IResultFile
+    {
+        public override SupportedFileType FileType => SupportedFileType.msp;
+        public override Software Software { get; set; }
+
+        public LibrarySpectrumFile() : base()
+        {
+        }
+
+        public LibrarySpectrumFile(string filePath) : base(filePath, Software.MetaMorpheus)
+        {
+        }
+
+        public override void LoadResults()
+        {
+            Results = MspSpectrumLibraryReader.ReadMsp(FilePath, out List<string> warnings);
+        }
+
+        public override void WriteResults(string outputPath) => throw new NotImplementedException();
+    }
+
+}
+
+
---
+++ mzLib/Readers/SpectrumLibraries/MspSpectrumLibraryReader.cs
@@ -0,0 +1,13 @@
+﻿using Omics.SpectrumMatch;
+
+namespace Readers.SpectrumLibraries
+{
+    internal class MspSpectrumLibraryReader
+    {
+        public static List<LibrarySpectrum> ReadMsp(string filePath, out List<string> warnings)
+        {
+            warnings = new List<string>();
+            return new List<LibrarySpectrum>();
+        }
+    }
+}
