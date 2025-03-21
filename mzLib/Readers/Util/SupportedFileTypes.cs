d78a48ea88278502d0317d5036d86a913b3c20ce
nbollis <nbollis@comcast.net>
2025-03-21 18:28:42 -0500

    added interlink to supported file expensions

--- mzLib/Readers/Util/SupportedFileTypes.cs
+++ mzLib/Readers/Util/SupportedFileTypes.cs
@@ -131,6 +131,8 @@ namespace Readers
                         return SupportedFileType.MsPathFinderTAllResults;
                     if(filePath.EndsWith(SupportedFileType.ExperimentAnnotation.GetFileExtension(), StringComparison.InvariantCultureIgnoreCase))
                         return SupportedFileType.ExperimentAnnotation;
+                    if (filePath.EndsWith(SupportedFileType.IntralinkResults.GetFileExtension(), StringComparison.InvariantCultureIgnoreCase))
+                        return SupportedFileType.IntralinkResults;
 
                     // these tsv cases are just .tsv and need an extra step to determine the type
                     // currently need to distinguish between FlashDeconvTsv and MsFraggerPsm
