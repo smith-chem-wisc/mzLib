d68a98b6340b784133949853a7ffd51498d0fcee
nbollis <nbollis@comcast.net>
2025-03-26 00:48:21 -0500

    Tested a ton of Variant databases

--- mzLib/Test/Transcriptomics/TestVariantOligo.cs
+++ mzLib/Test/Transcriptomics/TestVariantOligo.cs
@@ -1,7 +1,5 @@
 ﻿using NUnit.Framework;
 using Omics.BioPolymer;
-using Proteomics.ProteolyticDigestion;
-using Proteomics;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
@@ -11,6 +9,7 @@ using Transcriptomics;
 using Transcriptomics.Digestion;
 using UsefulProteomicsDatabases;
 using UsefulProteomicsDatabases.Transcriptomics;
+using Omics;
 
 namespace Test.Transcriptomics;
 
@@ -102,5 +101,201 @@ public class TestVariantOligo
         Assert.That(decoy.SequenceVariations.Single().OneBasedBeginPosition, Is.EqualTo(reversedModIdx));
         Assert.That(decoy.SequenceVariations.Single().OneBasedModifications.Count, Is.EqualTo(1));
         Assert.That(decoy.SequenceVariations.Single().OneBasedModifications.Single().Key, Is.EqualTo(reversedModIdx));
+    }
+
+    [TestCase("ranges1.xml", 1, 1, 5, 5)] // trunc excludes natural 3'
+    [TestCase("ranges2.xml", 2, 2, 6, 6)] // trunc includes natural 3'
+    public static void ReverseDecoyProteolysisProducts(string databaseName, int beginIdx, int reversedBeginIdx, int endIdx, int reversedEndIdx)
+    {
+        string dbPath = Path.Combine(TestContext.CurrentContext.TestDirectory, "Transcriptomics", "TestData", databaseName);
+        var rna = RnaDbLoader.LoadRnaXML(dbPath, true, DecoyType.Reverse, false, AllKnownMods, [], out var unknownModifications);
+        var target = rna[0];
+        Assert.That(target.TruncationProducts.Count(), Is.EqualTo(1));
+        Assert.That(target.TruncationProducts.Single().OneBasedBeginPosition, Is.EqualTo(beginIdx)); //[start]GUACU[end]G, G[start]UACUG[end]
+        Assert.That(target.TruncationProducts.Single().OneBasedEndPosition, Is.EqualTo(endIdx));
+        var decoy = rna[1];
+        Assert.That(decoy.TruncationProducts.Count(), Is.EqualTo(1));
+        Assert.That(decoy.TruncationProducts.Single().OneBasedBeginPosition, Is.EqualTo(reversedBeginIdx)); //DI[start]TPEP[end], M[start]DITP[end]E
+        Assert.That(decoy.TruncationProducts.Single().OneBasedEndPosition, Is.EqualTo(reversedEndIdx));
+
+        string rewriteDbName = $"{Path.GetFileNameWithoutExtension(databaseName)}rewrite.xml";
+        ProteinDbWriter.WriteXmlDatabase(null, rna.Where(p => !p.IsDecoy).ToList(), Path.Combine(TestContext.CurrentContext.TestDirectory, "Transcriptomics", "TestData", rewriteDbName));
+        rna = RnaDbLoader.LoadRnaXML(Path.Combine(TestContext.CurrentContext.TestDirectory, "Transcriptomics", "TestData", rewriteDbName), true,
+            DecoyType.Reverse, false, AllKnownMods, [], out unknownModifications);
+        target = rna[0];
+        Assert.That(target.TruncationProducts.Count(), Is.EqualTo(1));
+        Assert.That(target.TruncationProducts.Single().OneBasedBeginPosition, Is.EqualTo(beginIdx));
+        Assert.That(target.TruncationProducts.Single().OneBasedEndPosition, Is.EqualTo(endIdx));
+        decoy = rna[1];
+        Assert.That(decoy.TruncationProducts.Count(), Is.EqualTo(1));
+        Assert.That(decoy.TruncationProducts.Single().OneBasedBeginPosition, Is.EqualTo(reversedBeginIdx));
+        Assert.That(decoy.TruncationProducts.Single().OneBasedEndPosition, Is.EqualTo(reversedEndIdx));
+    }
+
+    [Test]
+    [TestCase("HomozygousHLA.xml", 1, 18)]
+    [TestCase("HomozygousHLA.xml", 10, 17)]
+    public static void HomozygousVariantsAtVariedDepths(string filename, int minVariantDepth, int appliedCount)
+    {
+        string dbPath = Path.Combine(TestContext.CurrentContext.TestDirectory, "Transcriptomics", "TestData", filename);
+        var rna = RnaDbLoader.LoadRnaXML(dbPath, true, DecoyType.None, false, AllKnownMods, [], out var unknownModifications, minAlleleDepth: minVariantDepth);
+        Assert.That(rna.Count, Is.EqualTo(1));
+        Assert.That(rna[0].SequenceVariations.Count(), Is.EqualTo(18)); // some redundant
+        Assert.That(rna[0].SequenceVariations.Select(v => v.SimpleString()).Distinct().Count(), Is.EqualTo(18)); // unique changes
+        Assert.That(rna[0].AppliedSequenceVariations.Count(), Is.EqualTo(appliedCount)); // some redundant
+        Assert.That(rna[0].AppliedSequenceVariations.Select(v => v.SimpleString()).Distinct().Count(), Is.EqualTo(appliedCount)); // unique changes
+        Assert.That(rna[0].GetVariantBioPolymers().Count, Is.EqualTo(1));
+        var variantProteins = rna[0].GetVariantBioPolymers();
+        List<OligoWithSetMods> peptides = rna.SelectMany(vp => vp.Digest(new RnaDigestionParams(), null, null)).ToList();
+    }
+
+    [Test]
+    public static void AppliedVariants()
+    {
+        ModificationMotif.TryGetMotif("C", out ModificationMotif motifP);
+        Modification mp = new Modification("mod", null, "type", null, motifP, "Anywhere.", null, 42.01, new Dictionary<string, IList<string>>(), null, null, null, null, null);
+
+        List<RNA> proteinsWithSeqVars = new List<RNA>
+            {
+                new RNA("GUACUGUA", "protein1", sequenceVariations: new List<SequenceVariation> { new SequenceVariation(4, 4, "C", "U", @"1\t50000000\t.\tA\tG\t.\tPASS\tANN=G||||||||||||||||\tGT:AD:DP\t1/1:30,30:30", null) }),
+                new RNA("GUACUGUA", "protein2", sequenceVariations: new List<SequenceVariation> { new SequenceVariation(4, 5, "CU", "AU", @"1\t50000000\t.\tA\tG\t.\tPASS\tANN=G||||||||||||||||\tGT:AD:DP\t1/1:30,30:30", null) }),
+                new RNA("GUACUGUA", "protein3", sequenceVariations: new List<SequenceVariation> { new SequenceVariation(4, 4, "C", "CCC", @"1\t50000000\t.\tA\tG\t.\tPASS\tANN=G||||||||||||||||\tGT:AD:DP\t1/1:30,30:30", null) }),
+                new RNA("GUACCCUGUA", "protein4", sequenceVariations: new List<SequenceVariation> { new SequenceVariation(4, 6, "CCC", "C", @"1\t50000000\t.\tA\tG\t.\tPASS\tANN=G||||||||||||||||\tGT:AD:DP\t1/1:30,30:30", null) }),
+                new RNA("GUACUGUA", "protein5", sequenceVariations: new List<SequenceVariation> { new SequenceVariation(4, 4, "C", "CCC", @"1\t50000000\t.\tA\tG\t.\tPASS\tANN=G||||||||||||||||\tGT:AD:DP\t1/1:30,30:30", new Dictionary<int, List<Modification>> {{ 5, new[] { mp }.ToList() } }) }),
+             };
+        var proteinsWithAppliedVariants = proteinsWithSeqVars.SelectMany(p => p.GetVariantBioPolymers()).ToList();
+        var proteinsWithAppliedVariants2 = proteinsWithSeqVars.SelectMany(p => p.GetVariantBioPolymers()).ToList(); // should be stable
+        string xml = Path.Combine(TestContext.CurrentContext.TestDirectory, "AppliedVariants.xml");
+        ProteinDbWriter.WriteXmlDatabase(null, proteinsWithSeqVars, xml);
+        var proteinsWithAppliedVariants3 = RnaDbLoader.LoadRnaXML(xml, true, DecoyType.None, false, AllKnownMods, null, out var un);
+
+        var listArray = new List<RNA>[] { proteinsWithAppliedVariants, proteinsWithAppliedVariants2, proteinsWithAppliedVariants3 };
+        for (int dbIdx = 0; dbIdx < listArray.Length; dbIdx++)
+        {
+            // sequences
+            Assert.That(listArray[dbIdx][0].BaseSequence, Is.EqualTo("GUAUUGUA"));
+            Assert.That(listArray[dbIdx][1].BaseSequence, Is.EqualTo("GUAAUGUA"));
+            Assert.That(listArray[dbIdx][2].BaseSequence, Is.EqualTo("GUACCCUGUA"));
+            Assert.That(listArray[dbIdx][3].BaseSequence, Is.EqualTo("GUACUGUA"));
+            Assert.That(listArray[dbIdx][4].BaseSequence, Is.EqualTo("GUACCCUGUA"));
+            Assert.That(listArray[dbIdx][4].OneBasedPossibleLocalizedModifications.Single().Key, Is.EqualTo(5));
+
+            // SAV
+            Assert.That(listArray[dbIdx][0].AppliedSequenceVariations.Single().OneBasedBeginPosition, Is.EqualTo(4));
+            Assert.That(listArray[dbIdx][0].AppliedSequenceVariations.Single().OneBasedEndPosition, Is.EqualTo(4));
+
+            // MNV
+            Assert.That(listArray[dbIdx][1].AppliedSequenceVariations.Single().OneBasedBeginPosition, Is.EqualTo(4));
+            Assert.That(listArray[dbIdx][1].AppliedSequenceVariations.Single().OneBasedEndPosition, Is.EqualTo(5));
+
+            // insertion
+            Assert.That(listArray[dbIdx][2].AppliedSequenceVariations.Single().OneBasedBeginPosition, Is.EqualTo(4));
+            Assert.That(listArray[dbIdx][2].AppliedSequenceVariations.Single().OneBasedEndPosition, Is.EqualTo(6));
+
+            // deletion
+            Assert.That(listArray[dbIdx][3].AppliedSequenceVariations.Single().OneBasedBeginPosition, Is.EqualTo(4));
+            Assert.That(listArray[dbIdx][3].AppliedSequenceVariations.Single().OneBasedEndPosition, Is.EqualTo(4));
+        }
+    }
+
+    [Test]
+    public static void AppliedVariants_AsBioPolymer()
+    {
+        ModificationMotif.TryGetMotif("C", out ModificationMotif motifP);
+        Modification mp = new Modification("mod", null, "type", null, motifP, "Anywhere.", null, 42.01, new Dictionary<string, IList<string>>(), null, null, null, null, null);
+
+        List<IBioPolymer> proteinsWithSeqVars = new List<IBioPolymer>
+            {
+                new RNA("GUACUGUA", "protein1", sequenceVariations: new List<SequenceVariation> { new SequenceVariation(4, 4, "C", "U", @"1\t50000000\t.\tA\tG\t.\tPASS\tANN=G||||||||||||||||\tGT:AD:DP\t1/1:30,30:30", null) }),
+                new RNA("GUACUGUA", "protein2", sequenceVariations: new List<SequenceVariation> { new SequenceVariation(4, 5, "CU", "AU", @"1\t50000000\t.\tA\tG\t.\tPASS\tANN=G||||||||||||||||\tGT:AD:DP\t1/1:30,30:30", null) }),
+                new RNA("GUACUGUA", "protein3", sequenceVariations: new List<SequenceVariation> { new SequenceVariation(4, 4, "C", "CCC", @"1\t50000000\t.\tA\tG\t.\tPASS\tANN=G||||||||||||||||\tGT:AD:DP\t1/1:30,30:30", null) }),
+                new RNA("GUACCCUGUA", "protein4", sequenceVariations: new List<SequenceVariation> { new SequenceVariation(4, 6, "CCC", "C", @"1\t50000000\t.\tA\tG\t.\tPASS\tANN=G||||||||||||||||\tGT:AD:DP\t1/1:30,30:30", null) }),
+                new RNA("GUACUGUA", "protein5", sequenceVariations: new List<SequenceVariation> { new SequenceVariation(4, 4, "C", "CCC", @"1\t50000000\t.\tA\tG\t.\tPASS\tANN=G||||||||||||||||\tGT:AD:DP\t1/1:30,30:30", new Dictionary<int, List<Modification>> {{ 5, new[] { mp }.ToList() } }) }),
+             };
+        var proteinsWithAppliedVariants = proteinsWithSeqVars.SelectMany(p => p.GetVariantBioPolymers()).ToList();
+        var proteinsWithAppliedVariants2 = proteinsWithSeqVars.SelectMany(p => p.GetVariantBioPolymers()).ToList(); // should be stable
+        string xml = Path.Combine(TestContext.CurrentContext.TestDirectory, "AppliedVariants.xml");
+        ProteinDbWriter.WriteXmlDatabase(null, proteinsWithSeqVars, xml);
+        var proteinsWithAppliedVariants3 = RnaDbLoader.LoadRnaXML(xml, true, DecoyType.None, false, AllKnownMods, null, out var un).Cast<IBioPolymer>().ToList();
+
+        var listArray = new List<IBioPolymer>[] { proteinsWithAppliedVariants, proteinsWithAppliedVariants2, proteinsWithAppliedVariants3 };
+        for (int dbIdx = 0; dbIdx < listArray.Length; dbIdx++)
+        {
+            // sequences
+            Assert.That(listArray[dbIdx][0].BaseSequence, Is.EqualTo("GUAUUGUA"));
+            Assert.That(listArray[dbIdx][1].BaseSequence, Is.EqualTo("GUAAUGUA"));
+            Assert.That(listArray[dbIdx][2].BaseSequence, Is.EqualTo("GUACCCUGUA"));
+            Assert.That(listArray[dbIdx][3].BaseSequence, Is.EqualTo("GUACUGUA"));
+            Assert.That(listArray[dbIdx][4].BaseSequence, Is.EqualTo("GUACCCUGUA"));
+            Assert.That(listArray[dbIdx][4].OneBasedPossibleLocalizedModifications.Single().Key, Is.EqualTo(5));
+
+            // SAV
+            Assert.That(listArray[dbIdx][0].AppliedSequenceVariations.Single().OneBasedBeginPosition, Is.EqualTo(4));
+            Assert.That(listArray[dbIdx][0].AppliedSequenceVariations.Single().OneBasedEndPosition, Is.EqualTo(4));
+
+            // MNV
+            Assert.That(listArray[dbIdx][1].AppliedSequenceVariations.Single().OneBasedBeginPosition, Is.EqualTo(4));
+            Assert.That(listArray[dbIdx][1].AppliedSequenceVariations.Single().OneBasedEndPosition, Is.EqualTo(5));
+
+            // insertion
+            Assert.That(listArray[dbIdx][2].AppliedSequenceVariations.Single().OneBasedBeginPosition, Is.EqualTo(4));
+            Assert.That(listArray[dbIdx][2].AppliedSequenceVariations.Single().OneBasedEndPosition, Is.EqualTo(6));
+
+            // deletion
+            Assert.That(listArray[dbIdx][3].AppliedSequenceVariations.Single().OneBasedBeginPosition, Is.EqualTo(4));
+            Assert.That(listArray[dbIdx][3].AppliedSequenceVariations.Single().OneBasedEndPosition, Is.EqualTo(4));
+        }
+    }
+
+    [Test]
+    public static void StopGained()
+    {
+
+        string dbPath = Path.Combine(TestContext.CurrentContext.TestDirectory, "Transcriptomics", "TestData", "StopGained.xml");
+        var rna = RnaDbLoader.LoadRnaXML(dbPath, true, DecoyType.None, false, AllKnownMods, [], out var unknownModifications);
+
+        Assert.That(rna.Count, Is.EqualTo(2));
+        Assert.That(rna[0].SequenceVariations.Count(), Is.EqualTo(1)); // some redundant
+        Assert.That(rna[0].SequenceVariations.Select(v => v.SimpleString()).Distinct().Count(), Is.EqualTo(1)); // unique changes
+        Assert.That(rna[0].AppliedSequenceVariations.Count(), Is.EqualTo(0)); // some redundant
+        Assert.That(rna[0].AppliedSequenceVariations.Select(v => v.SimpleString()).Distinct().Count(), Is.EqualTo(0)); // unique changes
+        Assert.That(rna[1].AppliedSequenceVariations.Count(), Is.EqualTo(1)); // some redundant
+        Assert.That(rna[1].AppliedSequenceVariations.Select(v => v.SimpleString()).Distinct().Count(), Is.EqualTo(1)); // unique changes
+        Assert.That(rna[0].Length, Is.EqualTo(191));
+        Assert.That(rna[0][161 - 1], Is.EqualTo('G'));
+        Assert.That(rna[1].Length, Is.EqualTo(161 - 1));
+        Assert.That(rna[0].Length, Is.Not.EqualTo(rna[1].Length));
+
+        rna = RnaDbLoader.LoadRnaXML(dbPath, true, DecoyType.None, false, AllKnownMods, [], out unknownModifications, minAlleleDepth: 400);
+
+        Assert.That(rna.Count, Is.EqualTo(1));
+        Assert.That(rna[0].AppliedSequenceVariations.Count(), Is.EqualTo(1)); // some redundant
+        Assert.That(rna[0].AppliedSequenceVariations.Select(v => v.SimpleString()).Distinct().Count(), Is.EqualTo(1)); // unique changes
+        Assert.That(rna[0].Length, Is.EqualTo(161 - 1));
+    }
+
+    [Test]
+    public static void MultipleAlternateAlleles()
+    {
+        string dbPath = Path.Combine(TestContext.CurrentContext.TestDirectory, "Transcriptomics", "TestData", "MultipleAlternateAlleles.xml");
+        var rna = RnaDbLoader.LoadRnaXML(dbPath, true, DecoyType.None, false, AllKnownMods, [], out var unknownModifications);
+        Assert.That(rna.Count, Is.EqualTo(2));
+        Assert.That(rna[0].SequenceVariations.Count(), Is.EqualTo(2)); // some redundant
+        Assert.That(rna[0].SequenceVariations.Select(v => v.SimpleString()).Distinct().Count(), Is.EqualTo(2)); // unique changes
+
+        Assert.That(rna[0].SequenceVariations.All(v => v.OneBasedBeginPosition == 63), Is.True); // there are two alternate alleles (1 and 2), but only 2 is in the genotype, so only that's applied
+        Assert.That(rna[1].AppliedSequenceVariations.Count(), Is.EqualTo(1)); // some redundant
+        Assert.That(rna[1].AppliedSequenceVariations.Select(v => v.SimpleString()).Distinct().Count(), Is.EqualTo(1)); // unique changes
+        Assert.That(rna[0].Length, Is.EqualTo(72));
+        Assert.That(rna[1].Length, Is.EqualTo(72));
+        Assert.That(rna[0][63 - 1], Is.EqualTo('G'));
+        Assert.That(rna[1][63 - 1], Is.EqualTo('A'));
+
+        rna = RnaDbLoader.LoadRnaXML(dbPath, true, DecoyType.None, false, AllKnownMods, [], out unknownModifications, minAlleleDepth: 10);
+
+        Assert.That(rna.Count, Is.EqualTo(1));
+        Assert.That(rna[0].AppliedSequenceVariations.Count(), Is.EqualTo(0)); // some redundant
+        Assert.That(rna[0].AppliedSequenceVariations.Select(v => v.SimpleString()).Distinct().Count(), Is.EqualTo(0)); // unique changes
+        Assert.That(rna[0][63 - 1], Is.EqualTo('G')); // reference only
     }
 }
