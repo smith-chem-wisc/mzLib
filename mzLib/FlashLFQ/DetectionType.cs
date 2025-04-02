954e638f9c1756cacd2fce2093c50221bee37b1a
Alexander-Sol <41119316+Alexander-Sol@users.noreply.github.com>
2025-04-02 18:21:21 -0500

    Update DetectionType.cs

--- mzLib/FlashLFQ/DetectionType.cs
+++ mzLib/FlashLFQ/DetectionType.cs
@@ -9,6 +9,6 @@
         IsoTrack_MBR,                   // The peak is detected from MBR by IsoTrack
         IsoTrack_Ambiguous,             // Multiple peptides are mapped to this peak by IsoTracker.
         MSMSIdentifiedButNotQuantified, // We have MS2ID but no peak for quantification
-        NotDetected,                    // We don't have MS2ID either peak for quantification, only for peptide quantification
+        NotDetected,                    // Peptide was not detected by MS2 or MBR. Only used in FlashLFQ Results when performing peptide-level quantification
     }
 }
