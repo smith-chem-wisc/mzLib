143d284c0916f1e0dc30c6544645cb4da09c7133
RayMSMS <150720362+RayMSMS@users.noreply.github.com>
2025-04-02 18:13:22 -0500

    Add the comment for NotDetected

--- mzLib/FlashLFQ/DetectionType.cs
+++ mzLib/FlashLFQ/DetectionType.cs
@@ -9,6 +9,6 @@
         IsoTrack_MBR,                   // The peak is detected from MBR by IsoTrack
         IsoTrack_Ambiguous,             // Multiple peptides are mapped to this peak by IsoTracker.
         MSMSIdentifiedButNotQuantified, // We have MS2ID but no peak for quantification
-        NotDetected,                    // We don't have MS2ID either peak for quantification
+        NotDetected,                    // We don't have MS2ID either peak for quantification, only for peptide quantification
     }
 }
