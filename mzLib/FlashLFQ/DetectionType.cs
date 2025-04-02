3743cc3306304f1aa70071c6b82d3dd01b3df9ae
RayMSMS <150720362+RayMSMS@users.noreply.github.com>
2025-04-02 17:55:01 -0500

    Add the comment for IsoTracker_Ambiguous

--- mzLib/FlashLFQ/DetectionType.cs
+++ mzLib/FlashLFQ/DetectionType.cs
@@ -7,7 +7,7 @@
         MBR,                            // The peak is detected from MBR
         MSMSAmbiguousPeakfinding,       // The peak is detected from more than one MS2ID
         IsoTrack_MBR,                   // The peak is detected from MBR by IsoTrack
-        IsoTrack_Ambiguous,             // The peak is detected from more than one MS2ID by IsoTrack
+        IsoTrack_Ambiguous,             // Multiple peptides are mapped to this peak by IsoTracker.
         MSMSIdentifiedButNotQuantified, // We have MS2ID but no peak for quantification
         NotDetected,                    // We don't have MS2ID either peak for quantification
     }
