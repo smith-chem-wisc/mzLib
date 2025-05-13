cbe10e8bbeb256c6014cc409deb2087e8119c0c4
nbollis <nbollis@comcast.net>
2025-05-13 17:32:17 -0500

    Broke out AverageResidue into its specific classes.

---
+++ mzLib/MassSpectrometry/Deconvolution/AverageResidue/AverageResidue.cs
@@ -0,0 +1,12 @@
+﻿namespace MassSpectrometry;
+
+public abstract class AverageResidue
+{
+    protected const double fineRes = 0.125;
+    protected const double minRes = 1e-8;
+    protected static readonly int NumAveraginesToGenerate = 1500;
+    public abstract int GetMostIntenseMassIndex(double testMass);
+    public abstract double[] GetAllTheoreticalMasses(int index);
+    public abstract double[] GetAllTheoreticalIntensities(int index);
+    public abstract double GetDiffToMonoisotopic(int index);
+}
