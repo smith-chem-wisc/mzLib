1aee26a53a55a57cdcf341a1fd6f8559265c7571
Zhuoxin Shi <zshi94@wisc.edu>
2025-06-13 13:23:48 -0500

    ExtractedIonChromatogram and XicSpline

---
+++ mzLib/MassSpectrometry/PeakIndexing/PeakSpline/CubicSpline.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using MathNet.Numerics.Interpolation;
+using MzLibUtil;
+
+namespace MassSpectrometry
+{
+    public class XicCubicSpline : XicSpline
+    {
+        public XicCubicSpline(double splineRtInterval = 0.05)
+        {
+            SplineRtInterval = splineRtInterval;
+        }
+
+        public override (double, double)[] GetSplineXYData(double[] rtArray, double[] intensityArray, double start, double end)
+        {
+            if (rtArray.Length != intensityArray.Length)
+            {
+                throw new MzLibException("Input arrays must have the same length.");
+            }
+            if (rtArray.Length < 5)
+            {
+                throw new MzLibException("Input arrays must contain at least 5 points.");
+            }
+            var cubicSpline = CubicSpline.InterpolateAkima(rtArray, intensityArray);
+            var XYData = CalculateSpline(start, end, SplineRtInterval, cubicSpline);
+
+            return XYData;
+        }
+    }
+}
--- mzLib/Readers/MsDataFileExtensions.cs
+++ mzLib/Readers/MsDataFileExtensions.cs
@@ -5,39 +5,6 @@ namespace Readers
 {
     public static class MsDataFileExtensions
     {
-        
-        /// <summary>
-        /// Extracts an ion chromatogram from the spectra file, given a mass, charge, retention time, and mass tolerance.
-        /// </summary>
-        public static ExtractedIonChromatogram ExtractIonChromatogram(this MsDataFile file, double neutralMass, int charge, Tolerance massTolerance,
-            double retentionTimeInMinutes, int msOrder = 1, double retentionTimeWindowWidthInMinutes = 5)
-        {
-            double theorMz = neutralMass.ToMz(charge);
-            double startRt = retentionTimeInMinutes - retentionTimeWindowWidthInMinutes / 2;
-            double endRt = retentionTimeInMinutes + retentionTimeWindowWidthInMinutes / 2;
-            List<Datum> xicData = new List<Datum>();
-
-            IEnumerable<MsDataScan> scansInRtWindow = file.GetMsScansInTimeRange(startRt, endRt);
-
-            foreach (MsDataScan scan in scansInRtWindow.Where(p => p.MsnOrder == msOrder))
-            {
-                int ind = scan.MassSpectrum.GetClosestPeakIndex(theorMz);
-
-                double expMz = scan.MassSpectrum.XArray[ind];
-
-                if (massTolerance.Within(expMz.ToMass(charge), neutralMass))
-                {
-                    xicData.Add(new Datum(scan.RetentionTime, scan.MassSpectrum.YArray[ind]));
-                }
-                else
-                {
-                    xicData.Add(new Datum(scan.RetentionTime, 0));
-                }
-            }
-
-            return new ExtractedIonChromatogram(xicData);
-        }
-
         /// <summary>
         /// Export any MsDataFile as an MzML file to a specific file location
         /// CAUTION: some software will check the NativeID for scan numbers
