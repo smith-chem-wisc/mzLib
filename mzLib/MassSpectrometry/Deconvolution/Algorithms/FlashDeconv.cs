da814093b544e279ec5d5bc09132ae0531e2077c
trishorts <mshort@chem.wisc.edu>
2025-06-23 12:29:25 -0500

    some helper methods within decon

--- mzLib/MassSpectrometry/Deconvolution/Algorithms/FlashDeconv.cs
+++ mzLib/MassSpectrometry/Deconvolution/Algorithms/FlashDeconv.cs
@@ -16,7 +16,8 @@ namespace MassSpectrometry
         }
         internal override IEnumerable<IsotopicEnvelope> Deconvolute(MzSpectrum spectrum, MzRange range)
         {
-            var t = LogTransformSpectrum(spectrum);
+            var logTransformedSpectrum = LogTransformSpectrum(spectrum);
+            var acceptibleLogMzDifferences = AllAcceptibleLogMzDifferences();
 
         }
         /// <summary>
@@ -67,11 +68,90 @@ namespace MassSpectrometry
                 currentId++;
             }
             return result;
-        }
-    
+        }   
         private MzSpectrum LogTransformSpectrum(MzSpectrum spectrum)
         {
             return new MzSpectrum(spectrum.XArray.Select(x => Math.Log(x)).ToArray(), spectrum.YArray, true);
+        }
+        private List<KeyValuePair<int, double>> AllAcceptibleLogMzDifferences(int lowValue = 1, int highValue = 60)
+        {
+            return Enumerable.Range(lowValue, highValue)
+                    .Select(i => new KeyValuePair<int, double>(i, Math.Log(i)))
+                    .ToList();
+        }
+        private List<(int mzDiffInt,double mzDiff)> Acceptible(int lowValue = 1, int highValue = 60)
+        {
+            return Enumerable.Range(lowValue, highValue)
+            .Select(i => (i, Math.Log(i)))
+            .ToList();
+        }
+        private List<(int, int)> FindIndexedLogMassDifferences(List<(int mzDiffInt, double mzDiff)> acceptibleLogMzDifferences, MzSpectrum spectrum, int selectedLogMzIndex)
+        {
+            double tolerance = 0.0001;
+            List<(int, int)> indexedLogMassDifferences = new ();
+            if(selectedLogMzIndex < spectrum.XArray.Length)
+            {
+                int logMzDiffIndex = 1;
+                while (logMzDiffIndex < acceptibleLogMzDifferences.Select(i=>i.mzDiffInt).Max())
+                {
+                    var i = FindIndexWithinTolerance(spectrum.XArray.ToList(), spectrum.XArray[selectedLogMzIndex] + acceptibleLogMzDifferences[logMzDiffIndex].mzDiff, tolerance);
+                    if (i >= 0)
+                    {
+                        indexedLogMassDifferences.Add((selectedLogMzIndex, i));
+                    }
+                    logMzDiffIndex++;
+                }
+
+            }
+            return indexedLogMassDifferences;
+
+
+        }
+        // Finds the index of the first value in a sorted list of doubles matching the target within a given tolerance
+        int FindIndexWithinTolerance(List<double> sortedList, double target, double tolerance)
+        {
+            for (int i = 0; i < sortedList.Count; i++)
+            {
+                if (Math.Abs(sortedList[i] - target) <= tolerance)
+                    return i;
+            }
+            return -1; // Not found
+        }
+        // Finds the longest sequence of consecutive integers in a sorted list
+        List<int> FindLongestConsecutiveSequence(List<int> sortedList)
+        {
+            if (sortedList == null || sortedList.Count == 0)
+                return new List<int>();
+
+            int maxStart = 0, maxLength = 1;
+            int currentStart = 0, currentLength = 1;
+
+            for (int i = 1; i < sortedList.Count; i++)
+            {
+                if (sortedList[i] == sortedList[i - 1] + 1)
+                {
+                    currentLength++;
+                }
+                else
+                {
+                    if (currentLength > maxLength)
+                    {
+                        maxLength = currentLength;
+                        maxStart = currentStart;
+                    }
+                    currentStart = i;
+                    currentLength = 1;
+                }
+            }
+
+            // Check at the end
+            if (currentLength > maxLength)
+            {
+                maxLength = currentLength;
+                maxStart = currentStart;
+            }
+
+            return sortedList.GetRange(maxStart, maxLength);
         }
     }
 }
