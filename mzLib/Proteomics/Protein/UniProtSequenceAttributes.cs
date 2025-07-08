a9bccccf0afefc86d2607f1927d7afe0e13bc79b
trishorts <mshort@chem.wisc.edu>
2025-07-08 11:07:31 -0500

    UniProt sequence attribute unit tests

--- mzLib/Proteomics/Protein/UniProtSequenceAttributes.cs
+++ mzLib/Proteomics/Protein/UniProtSequenceAttributes.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Proteomics.Protein
+namespace Proteomics
 {
     public class UniProtSequenceAttributes
     {
---
+++ mzLib/Test/UniProtSequenceAttributesTests.cs
@@ -0,0 +1,87 @@
+using System;
+using NUnit.Framework;
+using Proteomics;
+
+namespace Test
+{
+    [TestFixture]
+    [System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+    public class UniProtSequenceAttributesTests
+    {
+        [Test]
+        public void Constructor_SetsAllMandatoryProperties()
+        {
+            // Arrange
+            int length = 100;
+            int mass = 12345;
+            string checkSum = "ABC123";
+            DateTime entryModified = new DateTime(2024, 6, 13);
+            int sequenceVersion = 2;
+
+            // Act
+            var attr = new UniProtSequenceAttributes(length, mass, checkSum, entryModified, sequenceVersion);
+
+            // Assert
+            Assert.That(attr.Length, Is.EqualTo(length));
+            Assert.That(attr.Mass, Is.EqualTo(mass));
+            Assert.That(attr.CheckSum, Is.EqualTo(checkSum));
+            Assert.That(attr.EntryModified, Is.EqualTo(entryModified));
+            Assert.That(attr.SequenceVersion, Is.EqualTo(sequenceVersion));
+            Assert.That(attr.IsPrecursor, Is.Null);
+            Assert.That(attr.Fragment, Is.EqualTo(UniProtSequenceAttributes.FragmentType.unspecified));
+        }
+
+        [Test]
+        public void Constructor_SetsOptionalProperties()
+        {
+            // Arrange
+            int length = 200;
+            int mass = 54321;
+            string checkSum = "XYZ789";
+            DateTime entryModified = new DateTime(2023, 1, 1);
+            int sequenceVersion = 5;
+            bool? isPrecursor = true;
+            var fragment = UniProtSequenceAttributes.FragmentType.multiple;
+
+            // Act
+            var attr = new UniProtSequenceAttributes(length, mass, checkSum, entryModified, sequenceVersion, isPrecursor, fragment);
+
+            // Assert
+            Assert.That(attr.IsPrecursor, Is.EqualTo(isPrecursor));
+            Assert.That(attr.Fragment, Is.EqualTo(fragment));
+        }
+
+        [Test]
+        public void Properties_CanBeSetAndGet()
+        {
+            // Arrange
+            var attr = new UniProtSequenceAttributes(1, 2, "C", DateTime.Now, 3);
+
+            // Act
+            attr.Length = 10;
+            attr.Mass = 20;
+            attr.CheckSum = "DEF456";
+            attr.EntryModified = new DateTime(2022, 12, 31);
+            attr.SequenceVersion = 4;
+            attr.IsPrecursor = false;
+            attr.Fragment = UniProtSequenceAttributes.FragmentType.single;
+
+            // Assert
+            Assert.That(attr.Length, Is.EqualTo(10));
+            Assert.That(attr.Mass, Is.EqualTo(20));
+            Assert.That(attr.CheckSum, Is.EqualTo("DEF456"));
+            Assert.That(attr.EntryModified, Is.EqualTo(new DateTime(2022, 12, 31)));
+            Assert.That(attr.SequenceVersion, Is.EqualTo(4));
+            Assert.That(attr.IsPrecursor, Is.False);
+            Assert.That(attr.Fragment, Is.EqualTo(UniProtSequenceAttributes.FragmentType.single));
+        }
+
+        [Test]
+        public void FragmentType_Enum_Values_AreDistinct()
+        {
+            Assert.That(UniProtSequenceAttributes.FragmentType.single, Is.Not.EqualTo(UniProtSequenceAttributes.FragmentType.multiple));
+            Assert.That(UniProtSequenceAttributes.FragmentType.unspecified, Is.Not.EqualTo(UniProtSequenceAttributes.FragmentType.single));
+            Assert.That(UniProtSequenceAttributes.FragmentType.unspecified, Is.Not.EqualTo(UniProtSequenceAttributes.FragmentType.multiple));
+        }
+    }
+}
