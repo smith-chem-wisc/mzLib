be0fd0ff6384fe0ef66bef92efbb3dcb973bb408
trishorts <mshort@chem.wisc.edu>
2025-07-09 14:25:07 -0500

    unused usings

--- mzLib/Test/ProteinDbLoaderTests.cs
+++ mzLib/Test/ProteinDbLoaderTests.cs
@@ -1,12 +1,5 @@
 ﻿using NUnit.Framework;
-using Proteomics;
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml;
 using UsefulProteomicsDatabases;
 
 namespace Test
