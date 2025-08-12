b37435d743311db98413e553706c0477a12284f2
Alexander-Sol <41119316+Alexander-Sol@users.noreply.github.com>
2025-08-12 16:55:55 -0500

    Add interfaces for external decon results and implement for Ms1Feature/Dinosaur classes (#931)
    * Added SingleChargeMs1Feature interfaces, implementes for DinosaurTsv and Ms1Feature
    
    * expanded interface
    
    * Added tests
    
    ---------
    
    Co-authored-by: trishorts <mshort@chem.wisc.edu>

--- mzLib/Readers/ExternalResults/IndividualResultRecords/DinosaurFeature.cs
+++ mzLib/Readers/ExternalResults/IndividualResultRecords/DinosaurFeature.cs
@@ -8,8 +8,17 @@ namespace Readers
     /// <summary>
     /// A class representing a single entry in a .feature.tsv file, the output from Dinosaur
     /// </summary>
-    public class DinosaurFeature
+    public class DinosaurFeature : ISingleChargeMs1Feature
     {
+        #region IMs1FeatureProperties
+
+        public double RetentionTimeStart => RtStart;
+        public double RetentionTimeEnd => RtEnd;
+        public double Intensity => IntensityApex;
+        public int? NumberOfIsotopes => NIsotopes;
+
+        #endregion
+
         [Ignore]
         public static CsvConfiguration CsvConfiguration => new CsvConfiguration(CultureInfo.InvariantCulture)
         {
