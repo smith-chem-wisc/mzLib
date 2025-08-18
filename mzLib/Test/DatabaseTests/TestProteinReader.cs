50ec65cbb3dfe52ecf2daf8d95a0f8dc81f40423
Zhuoxin Shi <zshi94@wisc.edu>
2025-08-18 14:04:45 -0500

    TestSlideDecoyXML() in progress

--- mzLib/Test/DatabaseTests/TestProteinReader.cs
+++ mzLib/Test/DatabaseTests/TestProteinReader.cs
@@ -110,7 +110,7 @@ namespace Test.DatabaseTests
             Assert.AreEqual('M', ok[1][0]);
 
             Assert.AreEqual("P62805|H4_HUMAN|Histone H4", ok[0].FullDescription);
-            Assert.AreEqual("DECOY_P62805|H4_HUMAN|Histone H4", ok[1].FullDescription);
+            Assert.AreEqual("DECOY_P62805|H4_HUMAN|Histone H4", ok[2].FullDescription);
             Assert.AreEqual("ENST00000244537", ok[0].DatabaseReferences.First(dbRef => dbRef.Type == "Ensembl").Id);
             Assert.AreEqual("protein sequence ID", ok[0].DatabaseReferences.First(dbRef => dbRef.Type == "Ensembl").Properties.First().Item1);
             Assert.AreEqual("ENSP00000244537", ok[0].DatabaseReferences.First(dbRef => dbRef.Type == "Ensembl").Properties.First().Item2);
@@ -120,13 +120,13 @@ namespace Test.DatabaseTests
             Assert.AreEqual(23, ok[0].DatabaseReferences.Count(dbRef => dbRef.Type == "Ensembl"));
             Assert.AreEqual(0, ok[0].DisulfideBonds.Count());
             Assert.AreEqual(1, ok[0].SequenceVariations.Count());
-            Assert.AreEqual(1, ok[1].SequenceVariations.Count()); // decoys get the same sequence variations
+            Assert.AreEqual(1, ok[2].SequenceVariations.Count()); // decoys get the same sequence variations
             Assert.AreEqual(64, ok[0].SequenceVariations.First().OneBasedBeginPosition);
             Assert.AreEqual(64, ok[0].SequenceVariations.First().OneBasedEndPosition);
-            Assert.AreEqual(103 - 64 + 2, ok[1].SequenceVariations.First().OneBasedBeginPosition);
-            Assert.AreEqual(103 - 64 + 2, ok[1].SequenceVariations.First().OneBasedEndPosition);
-            Assert.AreNotEqual(ok[0].SequenceVariations.First().Description, ok[1].SequenceVariations.First().Description); //decoys and target variations don't have the same desc.
-            Assert.AreEqual("Homo sapiens", ok[1].Organism);
+            Assert.AreEqual(103 - 64 + 2, ok[2].SequenceVariations.First().OneBasedBeginPosition);
+            Assert.AreEqual(103 - 64 + 2, ok[2].SequenceVariations.First().OneBasedEndPosition);
+            Assert.AreNotEqual(ok[0].SequenceVariations.First().Description, ok[2].SequenceVariations.First().Description); //decoys and target variations don't have the same desc.
+            Assert.AreEqual("Homo sapiens", ok[2].Organism);
         }
 
         [Test]
@@ -190,7 +190,7 @@ namespace Test.DatabaseTests
             Assert.AreEqual('M', ok[1][0]);
 
             Assert.AreEqual("P62805|H4_HUMAN|Histone H4", ok[0].FullDescription);
-            Assert.AreEqual("DECOY_P62805|H4_HUMAN|Histone H4", ok[1].FullDescription);
+            Assert.AreEqual("DECOY_P62805|H4_HUMAN|Histone H4", ok[2].FullDescription);
             Assert.AreEqual("ENST00000244537", ok[0].DatabaseReferences.First(dbRef => dbRef.Type == "Ensembl").Id);
             Assert.AreEqual("protein sequence ID", ok[0].DatabaseReferences.First(dbRef => dbRef.Type == "Ensembl").Properties.First().Item1);
             Assert.AreEqual("ENSP00000244537", ok[0].DatabaseReferences.First(dbRef => dbRef.Type == "Ensembl").Properties.First().Item2);
