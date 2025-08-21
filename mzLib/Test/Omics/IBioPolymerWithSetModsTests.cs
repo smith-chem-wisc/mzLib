8c68f0773f694915dcf69c19dee498400d5ad2b1
Peter Cruz Parrilla <peter.cruzparrilla@gmail.com>
2025-08-21 15:12:37 -0500

    Updates full sequence writing methods to write C-terminus mods with a preceeding '-'. Updates the relevant tests as well.

--- mzLib/Test/Omics/IBioPolymerWithSetModsTests.cs
+++ mzLib/Test/Omics/IBioPolymerWithSetModsTests.cs
@@ -7,18 +7,21 @@ namespace Test.Omics;
 public class IBioPolymerWithSetModsTests
 {
     [Test]
-    [TestCase("AC[Phospho]DEFGH", "ACDEFGH", '[', ']')]
-    [TestCase("A[Oxidation]C[Phospho]DEFGH", "ACDEFGH", '[', ']')]
-    [TestCase("A[Oxidation]C[Phospho]D[Carbamidomethyl]EFGH", "ACDEFGH", '[', ']')]
-    [TestCase("A[Oxidation]C[Phospho]D[Carbamidomethyl]E[Acetyl]FGH", "ACDEFGH", '[', ']')]
-    [TestCase("A[Oxidation]C[Phospho]D[Carbamidomethyl]E[Acetyl]F[Phospho]GH", "ACDEFGH", '[', ']')]
-    [TestCase("A[Oxidation]C[Phospho]D[Carbamidomethyl]E[Acetyl]F[Phospho]G[Oxidation]H", "ACDEFGH", '[', ']')]
-    [TestCase("AC{Phospho}DEFGH", "ACDEFGH", '{', '}')]
-    [TestCase("A<Oxidation>C<Phospho>DEFGH", "ACDEFGH", '<', '>')]
-    [TestCase("A(Oxidation)C(Phospho)DEFGH", "ACDEFGH", '(', ')')]
-    public void TestGetBaseSequenceFromFullSequence(string fullSequence, string expectedBaseSequence, char startDelimiter, char endDelimiter)
+    [TestCase("AC[Phospho]DEFGH", "ACDEFGH", '[', ']', '-')]
+    [TestCase("A[Oxidation]C[Phospho]DEFGH", "ACDEFGH", '[', ']', '-')]
+    [TestCase("A[Oxidation]C[Phospho]D[Carbamidomethyl]EFGH", "ACDEFGH", '[', ']', '-')]
+    [TestCase("A[Oxidation]C[Phospho]D[Carbamidomethyl]E[Acetyl]FGH", "ACDEFGH", '[', ']', '-')]
+    [TestCase("A[Oxidation]C[Phospho]D[Carbamidomethyl]E[Acetyl]F[Phospho]GH", "ACDEFGH", '[', ']', '-')]
+    [TestCase("A[Oxidation]C[Phospho]D[Carbamidomethyl]E[Acetyl]F[Phospho]G[Oxidation]H", "ACDEFGH", '[', ']', '-')]
+    [TestCase("AC{Phospho}DEFGH", "ACDEFGH", '{', '}', '-')]
+    [TestCase("A<Oxidation>C<Phospho>DEFGH", "ACDEFGH", '<', '>', '-')]
+    [TestCase("A(Oxidation)C(Phospho)DEFGH", "ACDEFGH", '(', ')', '-')]
+    [TestCase("ACDEFGH-[TestMod: ModNameX]", "ACDEFGH", '[', ']', '-')]
+    [TestCase("ACDEFGH&[TestMod: ModNameX]", "ACDEFGH", '[', ']', '&')]
+    [TestCase("[TestMod: ModName]ACD[TestMod1: ModNameX]EFGH[TestMod2: ModNameY]-[TestMod3: ModNameZ]", "ACDEFGH", '[', ']', '-')]
+    public void TestGetBaseSequenceFromFullSequence(string fullSequence, string expectedBaseSequence, char startDelimiter, char endDelimiter, char cTerminusDelimiter)
     {
-        string actualBaseSequence = IBioPolymerWithSetMods.GetBaseSequenceFromFullSequence(fullSequence, startDelimiter, endDelimiter);
+        string actualBaseSequence = IBioPolymerWithSetMods.GetBaseSequenceFromFullSequence(fullSequence, startDelimiter, endDelimiter, cTerminusDelimiter);
         Assert.That(actualBaseSequence, Is.EqualTo(expectedBaseSequence));
     }
 }
