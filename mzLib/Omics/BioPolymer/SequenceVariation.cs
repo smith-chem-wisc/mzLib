36d4df23d21ec8089a58469c913bf534e7ad7901
trishorts <mshort@chem.wisc.edu>
2025-10-24 16:48:44 -0500

    u

--- mzLib/Omics/BioPolymer/SequenceVariation.cs
+++ mzLib/Omics/BioPolymer/SequenceVariation.cs
@@ -46,165 +46,77 @@ namespace Omics.BioPolymer
             OneBasedModifications = oneBasedModifications ?? new Dictionary<int, List<Modification>>();
         }
 
-        ///// <summary>
-        ///// For longer sequence variations, where a range of sequence is replaced. Point mutations should be specified with the same begin and end positions.
-        ///// </summary>
-        ///// <param name="oneBasedBeginPosition"></param>
-        ///// <param name="oneBasedEndPosition"></param>
-        ///// <param name="originalSequence"></param>
-        ///// <param name="variantSequence"></param>
-        ///// <param name="oneBasedModifications"></param>
-        //public SequenceVariation(int oneBasedBeginPosition, int oneBasedEndPosition, string originalSequence, string variantSequence, string description, Dictionary<int, List<Modification>>? oneBasedModifications = null)
-        //{
-        //    OneBasedBeginPosition = oneBasedBeginPosition;
-        //    OneBasedEndPosition = oneBasedEndPosition;
-        //    OriginalSequence = originalSequence ?? "";
-        //    VariantSequence = variantSequence ?? "";
-        //    VariantCallFormatDataString = new VariantCallFormat(description);
-        //    OneBasedModifications = oneBasedModifications ?? new Dictionary<int, List<Modification>>();
-        //}
-
-        /// <summary>
-        /// For variations with only position information (not begin and end).
-        /// Sets the end to the end of the original protein sequence to which this variation applies.
-        /// </summary>
-        /// <param name="oneBasedPosition"></param>
-        /// <param name="originalSequence"></param>
-        /// <param name="variantSequence"></param>
-        /// <param name="description"></param>
-        /// <param name="oneBasedModifications"></param>
         public SequenceVariation(int oneBasedPosition, string originalSequence, string variantSequence, string description, Dictionary<int, List<Modification>>? oneBasedModifications = null)
             : this(oneBasedPosition, originalSequence == null ? oneBasedPosition : oneBasedPosition + originalSequence.Length - 1, originalSequence, variantSequence, description, oneBasedModifications)
         { }
 
-        /// <summary>
-        /// Beginning position of original sequence to be replaced
-        /// </summary>
         public int OneBasedBeginPosition { get; }
-
-        /// <summary>
-        /// End position of original sequence to be replaced
-        /// </summary>
         public int OneBasedEndPosition { get; }
-
-        /// <summary>
-        /// Original sequence information (optional)
-        /// </summary>
         public string OriginalSequence { get; }
-
-        /// <summary>
-        /// Variant sequence information (required)
-        /// </summary>
         public string VariantSequence { get; }
         public string Description { get; }
-        /// <summary>
-        /// VariantCallFormatDataString of this variation (optional)
-        /// </summary>
         public VariantCallFormat? VariantCallFormatDataString { get; }
-
-
-        /// <summary>
-        /// Modifications specifically for this variant
-        /// </summary>
         public Dictionary<int, List<Modification>> OneBasedModifications { get; }
 
         public override bool Equals(object obj)
         {
             SequenceVariation s = obj as SequenceVariation;
             return s != null
-                   && OneBasedBeginPosition == s.OneBasedBeginPosition
-                   && OneBasedEndPosition == s.OneBasedEndPosition
-                   && (s.OriginalSequence == null && OriginalSequence == null || OriginalSequence.Equals(s.OriginalSequence))
-                   && (s.VariantSequence == null && VariantSequence == null || VariantSequence.Equals(s.VariantSequence))
-                   && ((s.VariantCallFormatDataString == null && VariantCallFormatDataString == null)
-                       || (VariantCallFormatDataString != null && VariantCallFormatDataString.Equals(s.VariantCallFormatDataString)))
-                   && (s.OneBasedModifications == null && OneBasedModifications == null ||
-                       s.OneBasedModifications.Keys.ToList().SequenceEqual(OneBasedModifications.Keys.ToList())
-                       && s.OneBasedModifications.Values.SelectMany(m => m).ToList().SequenceEqual(OneBasedModifications.Values.SelectMany(m => m).ToList()));
+                && OneBasedBeginPosition == s.OneBasedBeginPosition
+                && OneBasedEndPosition == s.OneBasedEndPosition
+                && (s.OriginalSequence == null && OriginalSequence == null || OriginalSequence.Equals(s.OriginalSequence))
+                && (s.VariantSequence == null && VariantSequence == null || VariantSequence.Equals(s.VariantSequence))
+                && ((s.VariantCallFormatDataString == null && VariantCallFormatDataString == null)
+                    || (VariantCallFormatDataString != null && VariantCallFormatDataString.Equals(s.VariantCallFormatDataString)))
+                && (s.OneBasedModifications == null && OneBasedModifications == null ||
+                    s.OneBasedModifications.Keys.ToList().SequenceEqual(OneBasedModifications.Keys.ToList())
+                    && s.OneBasedModifications.Values.SelectMany(m => m).ToList().SequenceEqual(OneBasedModifications.Values.SelectMany(m => m).ToList()));
         }
 
         public override int GetHashCode()
         {
             return OneBasedBeginPosition.GetHashCode()
-                   ^ OneBasedEndPosition.GetHashCode()
-                   ^ OriginalSequence.GetHashCode() // null handled in constructor
-                   ^ VariantSequence.GetHashCode() // null handled in constructor
-                   ^ (VariantCallFormatDataString?.GetHashCode() ?? Description.GetHashCode()); // fallback to non-null Description
+                ^ OneBasedEndPosition.GetHashCode()
+                ^ OriginalSequence.GetHashCode()
+                ^ VariantSequence.GetHashCode()
+                ^ (VariantCallFormatDataString?.GetHashCode() ?? 0);
         }
 
-        /// <summary>
-        /// Returns a simple string represantation of this amino acid change
-        /// </summary>
-        /// <returns></returns>
         public string SimpleString()
         {
             return OriginalSequence + OneBasedBeginPosition.ToString() + VariantSequence;
         }
 
-        /// <summary>
-        /// Determines whether this interval overlaps the queried interval
-        /// </summary>
-        /// <param name="segment"></param>
-        /// <returns></returns>
         internal bool Intersects(SequenceVariation segment)
         {
             return segment.OneBasedEndPosition >= OneBasedBeginPosition && segment.OneBasedBeginPosition <= OneBasedEndPosition;
         }
 
-        /// <summary>
-        /// Determines whether this interval overlaps the queried interval
-        /// </summary>
-        /// <param name="segment"></param>
-        /// <returns></returns>
         internal bool Intersects(TruncationProduct segment)
         {
             return segment.OneBasedEndPosition >= OneBasedBeginPosition && segment.OneBasedBeginPosition <= OneBasedEndPosition;
         }
 
-        /// <summary>
-        /// Determines whether this interval overlaps the queried position
-        /// </summary>
-        /// <param name="pos"></param>
-        /// <returns></returns>
         internal bool Intersects(int pos)
         {
             return OneBasedBeginPosition <= pos && pos <= OneBasedEndPosition;
         }
 
-        /// <summary>
-        /// Determines whether this interval includes the queried interval
-        /// </summary>
-        /// <param name="segment"></param>
-        /// <returns></returns>
         internal bool Includes(SequenceVariation segment)
         {
             return OneBasedBeginPosition <= segment.OneBasedBeginPosition && OneBasedEndPosition >= segment.OneBasedEndPosition;
         }
-        // Commented out by AVC on 4/5/23. Unused and untested in current code base,
-        // but can't rule out that it could be useful in the future.  
-        /// <summary>
-        /// Determines whether this interval includes the queried interval
-        /// </summary>
-        /// <param name="segment"></param>
-        /// <returns></returns>
-        // internal bool Includes(TruncationProduct segment)
-        // {
-        //     return OneBasedBeginPosition <= segment.OneBasedBeginPosition && OneBasedEndPosition >= segment.OneBasedEndPosition;
-        // }
 
-        /// <summary>
-        /// Determines whether this interval overlaps the queried position
-        /// </summary>
-        /// <param name="pos"></param>
-        /// <returns></returns>
         internal bool Includes(int pos)
         {
             return OneBasedBeginPosition <= pos && pos <= OneBasedEndPosition;
         }
+
         public bool AreValid()
         {
             return OneBasedBeginPosition > 0 && OneBasedEndPosition >= OneBasedBeginPosition;
         }
+
         private static bool LooksLikeVcf(string s)
             => !string.IsNullOrWhiteSpace(s)
                && (s.Contains("\t") || s.Contains("\\t"))
