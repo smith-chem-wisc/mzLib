137bfc149045722de451ee83ced8b0a3b5563715
trishorts <mshort@chem.wisc.edu>
2026-04-10 11:03:27 -0500

    Add XML documentation for Deconvoluter methods

--- mzLib/MassSpectrometry/Deconvolution/Deconvoluter.cs
+++ mzLib/MassSpectrometry/Deconvolution/Deconvoluter.cs
@@ -6,20 +6,41 @@ using System.Linq;
 
 namespace MassSpectrometry
 {
+    /// <summary>
+    /// Context class for all deconvolution
+    /// </summary>
     public static class Deconvoluter
     {
+        /// <summary>
+        /// Static deconvolution of an MsDataScan that does not require Deconvoluter construction
+        /// </summary>
+        /// <param name="scan">scan to deconvolute</param>
+        /// <param name="deconvolutionParameters">decon parameters to use, also determines type of deconvolution used</param>
+        /// <param name="rangeToGetPeaksFrom">Range of peaks to deconvolute, if null, will deconvolute entire spectra</param>
+        /// <returns></returns>
         public static IEnumerable<IsotopicEnvelope> Deconvolute(MsDataScan scan,
             DeconvolutionParameters deconvolutionParameters, MzRange rangeToGetPeaksFrom = null)
         {
             return Deconvolute(scan.MassSpectrum, deconvolutionParameters, rangeToGetPeaksFrom);
         }
 
+        /// <summary>
+        /// Static deconvolution of an MzSpectrum that does not require Deconvoluter construction
+        /// </summary>
+        /// <param name="spectrum">spectrum to deconvolute</param>
+        /// <param name="deconvolutionParameters">decon parameters to use, also determines type of deconvolution used</param>
+        /// <param name="rangeToGetPeaksFrom">Range of peaks to deconvolute, if null, will deconvolute entire spectra</param>
+        /// <returns></returns>
         public static IEnumerable<IsotopicEnvelope> Deconvolute(MzSpectrum spectrum,
             DeconvolutionParameters deconvolutionParameters, MzRange rangeToGetPeaksFrom = null)
         {
             rangeToGetPeaksFrom ??= spectrum.Range;
+            
+            // Short circuit deconvolution if it is called on a neutral mass spectrum
             if (spectrum is NeutralMassSpectrum newt)
                 return DeconvoluteNeutralMassSpectrum(newt, rangeToGetPeaksFrom);
+            
+            // set deconvolution algorithm 
             DeconvolutionAlgorithm deconAlgorithm = CreateAlgorithm(deconvolutionParameters);
             return deconAlgorithm.Deconvolute(spectrum, rangeToGetPeaksFrom);
         }
@@ -59,6 +80,12 @@ namespace MassSpectrometry
             };
         }
 
+        /// <summary>
+        /// Returns all peaks in the neutral mass spectrum as an isotopic envelope with a single peak
+        /// </summary>
+        /// <param name="neutralSpectrum"></param>
+        /// <param name="range"></param>
+        /// <returns></returns>
         private static IEnumerable<IsotopicEnvelope> DeconvoluteNeutralMassSpectrum(
             NeutralMassSpectrum neutralSpectrum, MzRange range)
         {
