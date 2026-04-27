596f7b42dd6d8a5c899d6a92413502b4449f0860
nbollis <nbollis@comcast.net>
2026-04-27 16:32:20 -0500

    ope

--- mzLib/Test/DictionaryPoolTests.cs
+++ mzLib/Test/DictionaryPoolTests.cs
@@ -4,7 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 
-namespace Test.MzLibUtil;
+namespace Test;
 
 [TestFixture]
 [ExcludeFromCodeCoverage]
--- mzLib/Test/HashSetPoolTests.cs
+++ mzLib/Test/HashSetPoolTests.cs
@@ -3,7 +3,7 @@ using NUnit.Framework;
 using System;
 using System.Diagnostics.CodeAnalysis;
 
-namespace Test.MzLibUtil;
+namespace Test;
 
 [TestFixture]
 [ExcludeFromCodeCoverage]
