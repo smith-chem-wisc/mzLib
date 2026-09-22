3a10349cbcec90a315ff33789bcbffaa62c3a4d2
nbollis <nbollis@comcast.net>
2026-09-22 18:35:54 -0500

    Dig: Terminal distinction

--- mzLib/Omics/Digestion/DigestionProduct.cs
+++ mzLib/Omics/Digestion/DigestionProduct.cs
@@ -188,7 +188,11 @@ namespace Omics.Digestion
 
             foreach (var entry in Parent.OneBasedFixedModifications)
             {
-                if (entry.Key >= OneBasedStartResidue && entry.Key <= OneBasedEndResidue)
+                if (entry.Key == 0 && OneBasedStartResidue == 1)
+                    fixedModsOneIsNterminus[1] = entry.Value;
+                else if (entry.Key == Parent.Length + 2 && OneBasedEndResidue == Parent.Length)
+                    fixedModsOneIsNterminus[length + 2] = entry.Value;
+                else if (entry.Key >= OneBasedStartResidue && entry.Key <= OneBasedEndResidue)
                     fixedModsOneIsNterminus[entry.Key - OneBasedStartResidue + 2] = entry.Value;
             }
         }
