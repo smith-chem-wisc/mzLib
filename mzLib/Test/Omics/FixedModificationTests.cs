d28885e0d2f3addafe508005f0cde9df2fc14b05
nbollis <nbollis@comcast.net>
2026-09-22 18:46:51 -0500

    more tests.

--- mzLib/Test/Omics/FixedModificationTests.cs
+++ mzLib/Test/Omics/FixedModificationTests.cs
@@ -1,9 +1,11 @@
 using System.Collections.Generic;
+using System.Linq;
 using Chemistry;
 using NUnit.Framework;
 using Omics.Modifications;
 using Proteomics;
 using Transcriptomics;
+using Transcriptomics.Digestion;
 
 namespace Test.Omics;
 
@@ -40,6 +42,36 @@ public class FixedModificationTests
 
         Assert.That(protein.OneBasedFixedModifications[1], Is.EqualTo(modification));
         Assert.That(clone.OneBasedFixedModifications[1], Is.EqualTo(modification));
+    }
+
+    [Test]
+    public void DigestedOligoExposesParentsOneBasedFixedModifications()
+    {
+        var modification = CreateModification('U');
+        var fixedModifications = new Dictionary<int, Modification>
+        {
+            [3] = modification,
+        };
+        var rna = new RNA("GUACUG", oneBasedFixedModifications: fixedModifications);
+
+        var digestedOligos = rna.Digest(
+                new RnaDigestionParams { MaxMods = 2 },
+                new List<Modification>(),
+                new List<Modification>())
+            .Cast<OligoWithSetMods>()
+            .ToList();
+
+        Assert.That(digestedOligos, Is.Not.Empty);
+        Assert.That(digestedOligos.All(oligo => oligo.OneBasedFixedModifications.ContainsKey(3)), Is.True);
+        Assert.That(digestedOligos.All(oligo => oligo.OneBasedFixedModifications[3] == modification), Is.True);
+    }
+
+    [Test]
+    public void ParentlessOligoReturnsEmptyOneBasedFixedModifications()
+    {
+        var parentlessOligo = new OligoWithSetMods("GUACUG");
+
+        Assert.That(parentlessOligo.OneBasedFixedModifications, Is.Empty);
     }
 
     private static Modification CreateModification(char target)
